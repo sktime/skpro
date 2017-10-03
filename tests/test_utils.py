b75d27f0719390a6e37f52ca13b1aa0121d7ef3b
Vitaly Davydov <1061040@gmail.com>
2017-10-03 18:03:39 +0100

    Requirements updated

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,7 +12,7 @@ def test_ecdf_from_sample(sample):
     xs, ys = ecdf(sample, return_func=False)
 
     assert len(xs) == len(ys)
-    # todo: assert xs, ys is the correct empircal cdf of the sample
+    # todo: assert xs, ys is the correct empirical cdf of the sample
 
     ecdf_func = ecdf(sample, return_func=True)
     # todo: assert ecdf is the correct ecdf step function
