8759f68a32efa14c7a455d4133c04c359ab03552
Vitaly Davydov <1061040@gmail.com>
2017-10-09 15:09:46 +0100

    Kernel density and empiric estimators + tests for pdf and cdf

--- skpro/density.py
+++ skpro/density.py
@@ -1,8 +1,9 @@
 import abc
-import numpy as np
 
-from sklearn.neighbors import KernelDensity
+import numpy as np
+from scipy.integrate import simps
 from sklearn.base import BaseEstimator
+from sklearn.neighbors import KernelDensity
 
 
 def ecdf(a):
@@ -19,7 +20,7 @@ def ecdf(a):
     array ys
     """
     xs = np.sort(np.array(a))
-    ys = np.arange(1, len(xs)+1)/float(len(xs))
+    ys = np.arange(1, len(xs) + 1) / float(len(xs))
 
     return xs, ys
 
@@ -43,7 +44,7 @@ def step_function(xs, ys):
         index = len(ys) - 1 if index >= len(ys) else index
         return ys[index]
 
-    return np.vectorize(func)
+    return func
 
 
 class DensityAdapter(BaseEstimator, metaclass=abc.ABCMeta):
@@ -112,14 +113,26 @@ class KernelDensityAdapter(DensityAdapter):
         """
 
         # fit kernel density estimator
+        self._min_sample = min(sample)
+        self._max_sample = max(sample)
+        self._std_sample = np.std(sample)
+
         self.estimator.fit(sample[:, np.newaxis])
 
     def cdf(self, x):
-        # TODO: integrate pdf
-        pass
+        a = 10
+        minus_inf = self._min_sample - a * self._std_sample
+
+        step = 0.01
+        grid = np.arange(minus_inf, x, step)
+
+        pdf_estimation = np.exp(self.estimator.score_samples(grid.reshape(-1, 1)))
+        integral = simps(y=pdf_estimation, dx=step)
+
+        return integral
 
     def pdf(self, x):
-        return np.exp(self.estimator.score_samples(x[:, np.newaxis]))
+        return np.exp(self.estimator.score_samples(x))[0]
 
 
 class EmpiricalDensityAdapter(DensityAdapter):
@@ -142,11 +155,32 @@ class EmpiricalDensityAdapter(DensityAdapter):
         np.array(M) inlet: Bayesian sample of length M
         """
         self.xs_, self.ys_ = ecdf(sample)
-        self.step_function_ = step_function(self.xs, self.ys)
+        self._hist, self._edges = np.histogram(
+            sample, bins='auto', density=True
+        )
+
+        self.step_function_ = step_function(self.xs_, self.ys_)
 
     def cdf(self, x):
-        return self.step_function_(x)
+        x_cdf = self.step_function_(x)
+
+        return x_cdf
 
     def pdf(self, x):
-        # TODO: integrate cdf stepfunction, manually using xs, ys, summing up the bars
-        pass
+        indexed_max = np.where(self._edges > x)[0]
+        if len(indexed_max) == 0:
+            return 1.0
+
+        i_max = min(indexed_max)
+        if i_max == 0:
+            return 0.0
+
+        pdf_sum = 0
+
+        for i in range(1, i_max):
+            bin_size = self._edges[i] - self._edges[i - 1]
+            pdf_sum += self._hist[i - 1] * bin_size
+
+        pdf_sum += self._hist[i_max - 1] * (x - self._edges[i_max - 1])
+
+        return pdf_sum
