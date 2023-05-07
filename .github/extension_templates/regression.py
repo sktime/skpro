f400ee1c78ed811b931221f7e3cd67d201f5f418
Franz Király <f.kiraly@ucl.ac.uk>
2023-05-07 23:04:07 +0100

    extension template

---
+++ .github/extension_templates/regression.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""Extension template for regressors."""
+# todo: write an informative docstring for the file or module, remove the above
+# todo: add an appropriate copyright notice for your estimator
+#       estimators contributed to sktime should have the copyright notice at the top
+#       estimators of your own do not need to have permissive or BSD-3 copyright
+
+# todo: uncomment the following line, enter authors' GitHub IDs
+# __author__ = [authorGitHubID, anotherAuthorGitHubID]
+
+from skpro.base import BaseProbaRegressor
+
+# todo: add any necessary imports here
+
+# todo: if any imports are sktime soft dependencies:
+# make sure to fill in the "python_dependencies" tag with the package import name
+
+
+# todo: change class name and write docstring
+class ClassName(BaseProbaRegressor):
+    """Custom probabilistic supervised regressor. todo: write docstring.
+
+    todo: describe your custom regressor here
+
+    Parameters
+    ----------
+    parama : int
+        descriptive explanation of parama
+    paramb : string, optional (default='default')
+        descriptive explanation of paramb
+    paramc : boolean, optional (default= whether paramb is not the default)
+        descriptive explanation of paramc
+    and so on
+    est : skpro.estimator, BaseEstimator descendant
+        descriptive explanation of est
+    est2: another estimator
+        descriptive explanation of est2
+    and so on
+    """
+
+    # todo: fill out estimator tags here
+    #  tags are inherited from parent class if they are not set
+    # todo: define the forecaster scitype by setting the tags
+    #  the "forecaster scitype" is determined by the tags
+    #   scitype:y - the expected input scitype of y - univariate or multivariate or both
+    #  when changing scitype:y to multivariate or both:
+    #   y_inner_mtype should be changed to pd.DataFrame
+    # other tags are "safe defaults" which can usually be left as-is
+    _tags = {}
+
+    # todo: fill init
+    # params should be written to self and never changed
+    # super call must not be removed, change class name
+    # parameter checks can go after super call
+    def __init__(self, paramname, paramname2="paramname2default"):
+        # estimators should precede parameters
+        #  if estimators have default values, set None and initalize below
+
+        # todo: write any hyper-parameters and components to self
+        self.paramname = paramname
+        self.paramname2 = "paramname2default"
+
+        # todo: change "ClassName" to the name of the class
+        super(ClassName, self).__init__()
+
+        # todo: optional, parameter checking logic (if applicable) should happen here
+        # if writes derived values to self, should *not* overwrite self.parama etc
+        # instead, write to self._parama, self._newparam (starting with _)
+
+        # todo: default estimators should have None arg defaults
+        #  and be initialized here
+        #  do this only with default estimators, not with parameters
+        # if est2 is None:
+        #     self.estimator = MyDefaultEstimator()
+
+        # todo: if tags of estimator depend on component tags, set these here
+        #  only needed if estimator is a composite
+        #  tags set in the constructor apply to the object and override the class
+        #
+        # example 1: conditional setting of a tag
+        # if est.foo == 42:
+        #   self.set_tags(handles-missing-data=True)
+        # example 2: cloning tags from component
+        #   self.clone_tags(est2, ["enforce_index_type", "handles-missing-data"])
+
+    # todo: implement this, mandatory
+    def _fit(self, X, y):
+        """Fit regressor to training data.
+
+        Writes to self:
+            Sets fitted model attributes ending in "_".
+
+        Parameters
+        ----------
+        X : pandas DataFrame
+            feature instances to fit regressor to
+        y : pandas DataFrame, must be same length as X
+            labels to fit regressor to
+
+        Returns
+        -------
+        self : reference to self
+        """
+        # insert logic for estimator here
+        # fitted parameters should be written to parameters ending in underscore
+
+        # self must be returned at the end
+        return self
+
+    # todo: implement this, mandatory
+    def _predict(self, X):
+        """Predict labels for data from features.
+
+        State required:
+            Requires state to be "fitted" = self.is_fitted=True
+
+        Accesses in self:
+            Fitted model attributes ending in "_"
+
+        Parameters
+        ----------
+        X : pandas DataFrame, must have same columns as X in `fit`
+            data to predict labels for
+
+        Returns
+        -------
+        y : pandas DataFrame, same length as `X`, same columns as `y` in `fit`
+            labels predicted for `X`
+        """
+        # implement logic for prediction here
+        # this can read out parameters fitted in fit, or hyperparameters from init
+        # no attributes should be written to self
+
+        y_pred = "placeholder"
+        # returned object should be pd.DataFrame
+        # same length as X, same columns as y in fit
+        return y_pred
+
+    # todo: implement this, mandatory
+    def _predict_proba(self, X):
+        """Predict distribution over labels for data from features.
+
+        State required:
+            Requires state to be "fitted".
+
+        Accesses in self:
+            Fitted model attributes ending in "_"
+
+        Parameters
+        ----------
+        X : pandas DataFrame, must have same columns as X in `fit`
+            data to predict labels for
+
+        Returns
+        -------
+        y : skpro BaseDistribution, same length as `X`
+            labels predicted for `X`
+        """
+        raise NotImplementedError
