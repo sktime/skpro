4fa5dc2bc37e595a2ba413ca1e33c8bffa1db0cb
Franz Király <f.kiraly@ucl.ac.uk>
2023-05-09 02:22:57 +0100

    test

--- skpro/tests/test_all_estimators.py
+++ skpro/tests/test_all_estimators.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 """Automated tests based on the skbase test suite template."""
+import numbers
+import types
+from inspect import getfullargspec, signature
+
+import numpy as np
+
 from skbase.testing import TestAllObjects as _TestAllObjects
+from skbase.testing.utils.inspect import _get_args
 
 
 class PackageConfig:
@@ -20,6 +27,7 @@ class PackageConfig:
     # list of valid tags
     # expected type: list of str, str are tag names
     valid_tags = [
+        "reserved_params",
         "estimator_type",
         "python_version",
         "python_dependencies",
@@ -33,3 +41,90 @@ class PackageConfig:
 
 class TestAllObjects(PackageConfig, _TestAllObjects):
     """Generic tests for all objects in the mini package."""
+
+    # override this due to reserved_params index, columns, in the BaseDistribution class
+    # index and columns params behave like pandas, i.e., are changed after __init__
+    def test_constructor(self, object_class):
+        """Check that the constructor has sklearn compatible signature and behaviour.
+
+        Based on sklearn check_estimator testing of __init__ logic.
+        Uses create_test_instance to create an instance.
+        Assumes test_create_test_instance has passed and certified create_test_instance.
+
+        Tests that:
+        * constructor has no varargs
+        * tests that constructor constructs an instance of the class
+        * tests that all parameters are set in init to an attribute of the same name
+        * tests that parameter values are always copied to the attribute and not changed
+        * tests that default parameters are one of the following:
+            None, str, int, float, bool, tuple, function, joblib memory, numpy primitive
+            (other type parameters should be None, default handling should be by writing
+            the default to attribute of a different name, e.g., my_param_ not my_param)
+        """
+        msg = "constructor __init__ should have no varargs"
+        assert getfullargspec(object_class.__init__).varkw is None, msg
+
+        estimator = object_class.create_test_instance()
+        assert isinstance(estimator, object_class)
+
+        # Ensure that each parameter is set in init
+        init_params = _get_args(type(estimator).__init__)
+        invalid_attr = set(init_params) - set(vars(estimator)) - {"self"}
+        assert not invalid_attr, (
+            "Estimator %s should store all parameters"
+            " as an attribute during init. Did not find "
+            "attributes `%s`." % (estimator.__class__.__name__, sorted(invalid_attr))
+        )
+
+        # Ensure that init does nothing but set parameters
+        # No logic/interaction with other parameters
+        def param_filter(p):
+            """Identify hyper parameters of an estimator."""
+            return p.name != "self" and p.kind not in [p.VAR_KEYWORD, p.VAR_POSITIONAL]
+
+        init_params = [
+            p
+            for p in signature(estimator.__init__).parameters.values()
+            if param_filter(p)
+        ]
+
+        params = estimator.get_params()
+
+        test_params = object_class.get_test_params()
+        if isinstance(test_params, list):
+            test_params = test_params[0]
+        test_params = test_params.keys()
+
+        init_params = [param for param in init_params if param.name not in test_params]
+
+        for param in init_params:
+            assert param.default != param.empty, (
+                "parameter `%s` for %s has no default value and is not "
+                "set in `get_test_params`" % (param.name, estimator.__class__.__name__)
+            )
+            if type(param.default) is type:
+                assert param.default in [np.float64, np.int64]
+            else:
+                assert type(param.default) in [
+                    str,
+                    int,
+                    float,
+                    bool,
+                    tuple,
+                    type(None),
+                    np.float64,
+                    types.FunctionType,
+                ]
+
+            reserved_params = object_class.get_class_tag("reserved_params", [])
+            if param.name not in reserved_params:
+                param_value = params[param.name]
+                if isinstance(param_value, np.ndarray):
+                    np.testing.assert_array_equal(param_value, param.default)
+                elif bool(
+                    isinstance(param_value, numbers.Real) and np.isnan(param_value)
+                ):
+                    # Allows to set default parameters to np.nan
+                    assert param_value is param.default, param.name
+                else:
+                    assert param_value == param.default, param.name
