891a7b60e30e0114c7e8cb5322a7c7ee8ac8fcb3
Franz Király <f.kiraly@ucl.ac.uk>
2023-05-13 14:47:25 +0100

    linting

--- skpro/distributions/tests/test_all_distrs.py
+++ skpro/distributions/tests/test_all_distrs.py
@@ -8,7 +8,6 @@ __author__ = ["fkiraly"]
 import numpy as np
 import pandas as pd
 import pytest
-
 from skbase.testing import BaseFixtureGenerator, QuickTester
 
 from skpro.distributions.base import BaseDistribution
