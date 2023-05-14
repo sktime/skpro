20a312eda64e0a60ba76f50110cfb40810494950
Franz Király <f.kiraly@ucl.ac.uk>
2023-05-14 20:13:21 +0100

    [ENH] test suite and method defaults for distributions (#12)
    This PR adds a test suite for distributions to `skpro`, based on the
    `scikit-base` fixture generation factory and the current suite tests in
    `sktime`.
    
    Also includes `sktime` https://github.com/sktime/sktime/pull/4583
    adapted to `skpro`:
    
    * adds test coverage for method implementation defaults, e.g., using
    `sample` to compute the mean if no exact implementation is available
    * refactors existing defaults to be conditional on
    `_has_implementation`_of
    * fixes bugs in `mean`, `var` defaults, which were using the wrong
    `groupby` level
    * adds a default for `sample`, based on inverse transform sampling if
    `ppf` is available

---
+++ skpro/distributions/tests/test_proba_basic.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Non-suite tests for probability distribution objects."""
+# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
+# adapted from sktime
+
+__author__ = ["fkiraly"]
+
+
+def test_proba_example():
+    """Test one subsetting case for BaseDistribution."""
+    from skpro.distributions.normal import Normal
+
+    n = Normal(mu=[[0, 1], [2, 3], [4, 5]], sigma=1)
+
+    assert n.shape == (3, 2)
+
+    one_row = n.loc[[1]]
+    assert isinstance(one_row, Normal)
+    assert one_row.shape == (1, 2)
