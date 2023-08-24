b8ad5eb4e251e7d3decfc441ebabddc4447a077f
Franz Király <f.kiraly@ucl.ac.uk>
2023-08-24 21:41:59 +0100

    export, docstring example

--- skpro/distributions/__init__.py
+++ skpro/distributions/__init__.py
@@ -3,7 +3,8 @@
 # copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
 # adapted from sktime
 
-__all__ = ["Laplace", "Normal"]
+__all__ = ["Empirical", "Laplace", "Normal"]
 
+from skpro.distributions.empirical import Empirical
 from skpro.distributions.laplace import Laplace
 from skpro.distributions.normal import Normal
