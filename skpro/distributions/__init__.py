7e1aac86dece2f9713e47fe2bdc44e4bcc713831
Franz Király <f.kiraly@ucl.ac.uk>
2023-08-25 21:25:41 +0100

    exports

--- skpro/distributions/__init__.py
+++ skpro/distributions/__init__.py
@@ -3,7 +3,12 @@
 # copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
 # adapted from sktime
 
-__all__ = ["Laplace", "Normal"]
+__all__ = [
+    "Laplace",
+    "Mixture",
+    "Normal",
+]
 
 from skpro.distributions.laplace import Laplace
+from skpro.distributions.normal import Mixture
 from skpro.distributions.normal import Normal
