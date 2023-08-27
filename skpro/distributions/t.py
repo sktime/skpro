6bb62c4af8128f1d86f0fe5004535d2f04a709b8
Franz Király <f.kiraly@ucl.ac.uk>
2023-08-27 10:55:00 +0100

    Update t.py

--- skpro/distributions/t.py
+++ skpro/distributions/t.py
@@ -27,7 +27,7 @@ class TDistribution(BaseDistribution):
 
     Example
     -------
-    >>> from skpro.distriutions.t import TDistribution
+    >>> from skpro.distributions.t import TDistribution
 
     >>> n = TDistribution(mu=[[0, 1], [2, 3], [4, 5]], sigma=1, df=10)
     """
