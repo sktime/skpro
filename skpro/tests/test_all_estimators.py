6129c9af1d1c4a5897fc4d0d0f3bb59c966c48f5
Franz Király <f.kiraly@ucl.ac.uk>
2023-10-08 20:13:20 +0200

    Update test_all_estimators.py

--- skpro/tests/test_all_estimators.py
+++ skpro/tests/test_all_estimators.py
@@ -90,7 +90,8 @@ class BaseFixtureGenerator(_BaseFixtureGenerator):
 
         # this setting ensures that only estimators are tested that have changed
         # in the sense that any line in the module is different from main
-        if ONLY_CHANGED_MODULES:
+        # exception: if this module has changed, we always run all tests by override
+        if ONLY_CHANGED_MODULES and not is_class_changed(type(self)):
             obj_list = [obj for obj in obj_list if is_class_changed(obj)]
 
         return obj_list
