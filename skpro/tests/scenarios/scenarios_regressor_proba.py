ffbde521dea534a0ee635175b67fc1c0ef393d66
Franz Király <franzkiraly@inf.elte.hu>
2023-11-12 22:09:30 +0100

    [ENH] scenario tests for mixed `pandas` column index types (#145)
    This PR adds a scenario test for probabilistic regressors to cover:
    
    * mixed column index types int and str - `sklearn` cannot deal with
    this, so this can be a likely breakage condition for estimators that
    wrap sklearn estimators
    * 1D numpy `y`
    
    Depends on the following fixes to bugs covered by the change:
    
    * https://github.com/sktime/skpro/pull/146
    * https://github.com/sktime/skpro/pull/148
    
    Was tested with incremental testing turned off, all remaining estimators pass.

--- skpro/tests/scenarios/scenarios_regressor_proba.py
+++ skpro/tests/scenarios/scenarios_regressor_proba.py
@@ -72,17 +72,36 @@ class _ProbaRegressorTestScenario(TestScenario, BaseObject):
         return True
 
 
-# as a function to ensure we can move to fixture-like structure later
-def _get_Xy_traintest():
+# as functions to ensure we can move to fixture-like structure later
+def _load_diabetes_small():
+    """Load diabetes dataset, return first 50 samples."""
     import pandas as pd
     from sklearn.datasets import load_diabetes
-    from sklearn.model_selection import train_test_split
 
     X, y = load_diabetes(return_X_y=True, as_frame=True)
     X = X.iloc[:50]
     y = y.iloc[:50]
     y = pd.DataFrame(y)
 
+    return X, y
+
+
+def _get_Xy_traintest():
+    """Get X, y train/test data for regression tests.
+
+    X, y are pandas.DataFrame, y is univariate.
+    X has homogeneous dtype, and no missing data.
+
+    Returns
+    -------
+    X_train : pandas.DataFrame, training data
+    X_test : pandas.DataFrame, test data
+    y_train : pandas.DataFrame, training labels
+    y_test : pandas.DataFrame, test labels
+    """
+    from sklearn.model_selection import train_test_split
+
+    X, y = _load_diabetes_small()
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
     return X_train, X_test, y_train, y_test
@@ -92,7 +111,7 @@ X_train, X_test, y_train, y_test = _get_Xy_traintest()
 
 
 class ProbaRegressorBasic(_ProbaRegressorTestScenario):
-    """Fit/predict with univariate panel X, numpy3D mtype, and labels y."""
+    """Fit/predict with multivariate pandas mtype (same dtype), and labels y."""
 
     _tags = {
         "X_univariate": False,
@@ -111,4 +130,59 @@ class ProbaRegressorBasic(_ProbaRegressorTestScenario):
 
 scenarios_regressor_proba = [
     ProbaRegressorBasic,
+]
+
+
+# as a function to ensure we can move to fixture-like structure later
+def _get_Xy_traintest_X_mixix_ynp():
+    """Get X, y train/test data for regression tests.
+
+    X is pandas.DataFrame, y is univariate numpy array.
+    X has homogeneous dtype, and no missing data.
+
+    X has mixed column index type (int and str).
+
+    Returns
+    -------
+    X_train : pandas.DataFrame, training data
+    X_test : pandas.DataFrame, test data
+    y_train : pandas.DataFrame, training labels
+    y_test : pandas.DataFrame, test labels
+    """
+    from sklearn.model_selection import train_test_split
+
+    X, y = _load_diabetes_small()
+
+    X.columns = ["age", "sex", "bmi", "bp", 1, 2, 3, 4, 5, 6]
+    y = y.values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    return X_train, X_test, y_train, y_test
+
+
+X_train_mc, X_test_mc, y_train_mc, y_test_mc = _get_Xy_traintest_X_mixix_ynp()
+
+
+class ProbaRegressorXcolMixIxYnp(_ProbaRegressorTestScenario):
+    """Fit/predict with multivariate pandas mtype, mixed col idx type."""
+
+    _tags = {
+        "X_univariate": False,
+        "X_missing": False,
+        "y_univariate": True,
+        "is_enabled": True,
+    }
+
+    args = {
+        "fit": {"X": X_train_mc, "y": y_train_mc},
+        "predict": {"X": X_test_mc},
+    }
+    default_method_sequence = ["fit", "predict", "predict_proba"]
+    default_arg_sequence = ["fit", "predict", "predict"]
+
+
+scenarios_regressor_proba = [
+    ProbaRegressorBasic,
+    ProbaRegressorXcolMixIxYnp,
 ]
