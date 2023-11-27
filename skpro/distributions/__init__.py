075b1f5fc7ce529cfd2c6aa4e275281a37f451ec
setoguchi-naoki <setoguchi.naoki@jp.panasonic.com>
2023-11-27 10:38:01 +0900

    [add] import qpd_B

--- skpro/distributions/__init__.py
+++ skpro/distributions/__init__.py
@@ -2,11 +2,20 @@
 # copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
 # adapted from sktime
 
-__all__ = ["Empirical", "Laplace", "Mixture", "Normal", "TDistribution", "J_QPD_S"]
+__all__ = [
+    "Empirical",
+    "Laplace",
+    "Mixture",
+    "Normal",
+    "TDistribution",
+    "QPD_S",
+    "QPD_B",
+]
 
 from skpro.distributions.empirical import Empirical
 from skpro.distributions.laplace import Laplace
 from skpro.distributions.mixture import Mixture
 from skpro.distributions.normal import Normal
 from skpro.distributions.t import TDistribution
-from skpro.distributions.qpd import J_QPD_S
+from skpro.distributions.qpd import QPD_S
+from skpro.distributions.qpd import QPD_B
