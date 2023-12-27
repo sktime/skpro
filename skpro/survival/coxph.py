a0e16a810d3c78aa9c01cfbcc9a497cdf634fb69
Franz Király <f.kiraly@ucl.ac.uk>
2023-12-27 22:39:30 +0100

    start working on coxph

---
+++ skpro/survival/coxph.py
@@ -0,0 +1,216 @@
+"""Interface adapter statsmodels cox proportional hazards."""
+# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
+
+__author__ = ["fkiraly"]
+
+import numpy as np
+import pandas as pd
+
+from skpro.regression.base import BaseProbaRegressor
+
+
+class CoxPH(BaseProbaRegressor):
+    """MAPIE probabilistic regressor, conformity score based prediction intervals.
+
+    Direct interface to ``mapie.regression.regression.MapieRegressor`` from the
+    ``mapie`` package.
+
+    Uses jackknife+ to estimate prediction intervals on a per-sample basis.
+
+    Any (non-probabilistic) sklearn regressor can be used as the base regressor,
+    ``MapieRegressor`` will add prediction intervals.
+
+    Parameters
+    ----------
+    method : str, optional (default="lpl"), one of {"lpl", "elastic_net}
+        Method used to fit the proportional hazards model.
+        "lpl": log partial likelihood minimization, vanilla method.
+        Corresponds to statsmodels PHReg.fit method.
+        "elastic_net": elastic net regularization.
+        Corresponds to statsmodels PHReg.fit_regularized method.
+
+    ties : str, optional (default="breslow"), one of {"breslow", "efron"}
+        Method used to deal with ties in the data.
+        "breslow": Breslow's method.
+        "efron": Efron's method.
+    missing : str, optional (default="drop"), one of {"drop"}
+        Method used to deal with missing data, statsmodels native.
+        "drop": Drops all observations with missing data.
+        Note: statsmodels currently supports only "drop" for missing data.
+        For imputation, pipeline with sklearn.impute.SimpleImputer,
+        or another sklearn composable imputer, via skpro Pipeline (or * dunder)
+    strata : pd.Index element or coercible, optional (default=None)
+        Strata to use for stratified estimation.
+        loc column reference to X, to column to be used as strata (categorical)
+        If None, no stratification is used.
+        If not None, must be a single valid loc column index of X.
+        Iterable is used in loc subsetting of X, i.e., X.loc[:, [strata]].
+        Corresponds to statsmodels PHReg.strata parameter.
+    alpha : float or iterable of float, optional (default=0.0)
+        Used only if method="elastic_net", otherwise ignored.
+        Regularization parameter for elastic net regularization.
+        If float, regularization parameter is the same for all variables.
+        If iterable, must be of same length as number of variables (columns) in X.
+        Corresponds to statsmodels PHReg.fit_regularized alpha parameter.
+        
+    Attributes
+    ----------
+    estimator_: EnsembleRegressor
+        fitted mapie ensemble regressor
+
+    conformity_scores_: ArrayLike of shape (n_samples_train,)
+        Conformity scores between ``y_train`` and ``y_pred``.
+
+    Example
+    -------
+    >>> from skpro.regression.mapie import MapieRegressor  # doctest: +SKIP
+    >>> from sklearn.ensemble import RandomForestRegressor  # doctest: +SKIP
+    >>> from sklearn.datasets import load_diabetes  # doctest: +SKIP
+    >>> from sklearn.model_selection import train_test_split  # doctest: +SKIP
+    >>>
+    >>> X, y = load_diabetes(return_X_y=True, as_frame=True)  # doctest: +SKIP
+    >>> X_train, X_test, y_train, y_test = train_test_split(X, y)  # doctest: +SKIP
+    >>>
+    >>> reg_tabular = RandomForestRegressor()  # doctest: +SKIP
+    >>>
+    >>> reg_proba = MapieRegressor(reg_tabular)  # doctest: +SKIP
+    >>> reg_proba.fit(X_train, y_train)  # doctest: +SKIP
+    MapieRegressor(...)
+    >>> y_pred_int = reg_proba.predict_interval(X_test)  # doctest: +SKIP
+    >>> y_pred_dist = reg_proba.predict_proba(X_test)  # doctest: +SKIP
+    """
+
+    _tags = {
+        "capability:missing": False,
+        "capability:survival": True,
+        "python_dependencies": ["statsmodels"],
+    }
+
+    def __init__(
+        self,
+        ties="breslow",
+        missing="drop",
+        strata=None,
+        alpha=0.0,
+    ):
+        self.ties = ties
+        self.missing = missing
+        self.strata = strata
+        self.alpha = alpha
+        super().__init__()
+
+    def _fit(self, X, y, C=None):
+        """Fit regressor to training data.
+
+        Writes to self:
+            Sets fitted model attributes ending in "_".
+
+        Parameters
+        ----------
+        X : pandas DataFrame
+            feature instances to fit regressor to
+        y : pandas DataFrame, must be same length as X
+            labels to fit regressor to
+        C : pd.DataFrame, optional (default=None)
+            censoring information for survival analysis,
+            should have same column name as y, same length as X and y
+            should have entries 0 and 1 (float or int)
+            0 = uncensored, 1 = (right) censored
+            if None, all observations are assumed to be uncensored
+            Can be passed to any probabilistic regressor,
+            but is ignored if capability:survival tag is False.
+
+        Returns
+        -------
+        self : reference to self
+        """
+        from statsmodels.duration.hazard_regression import PHReg
+
+        endog = y.to_numpy().flatten()
+        exog = X.to_numpy()
+        status = C.to_numpy().flatten() if C is not None else None
+
+        params = {
+            "ties": self.ties,
+            "missing": self.missing,
+        }
+
+        model = PHReg(endog=endog, exog=exog, status=status, **params)
+
+        # fit model
+        if self.method == "lpl":
+            self._model = model.fit()
+        else:
+            self._model = model.fit_regularized(method=self.method, alpha=self.alpha)
+
+        return self
+
+    def _predict_proba(self, X):
+        """Predict distribution over labels for data from features.
+
+        State required:
+            Requires state to be "fitted".
+
+        Accesses in self:
+            Fitted model attributes ending in "_"
+
+        Parameters
+        ----------
+        X : pandas DataFrame, must have same columns as X in `fit`
+            data to predict labels for
+
+        Returns
+        -------
+        y_pred : skpro BaseDistribution, same length as `X`
+            labels predicted for `X`
+        """
+        from skpro.distributions.adapters import
+
+        # boilerplate code to create correct output index
+        index = X.index
+        y_cols = self._y_cols  # columns from y in fit, not automatically stored
+        columns = y_cols
+
+        # values = logic to produce prediction values
+        # replace this import by the distribution you are using
+        # the distribution type can be conditional, e.g., data or parameter dependent
+        from skpro.distributions import SomeDistribution
+
+        values = None  # fill in values
+        y_pred = SomeDistribution(values, index=index, columns=columns)
+
+        return y_pred
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`
+        """
+        from sklearn.ensemble import RandomForestRegressor
+
+        params1 = {}
+
+        params2 = {
+            "estimator": RandomForestRegressor(),
+            "method": "base",
+            "cv": 2,
+            "test_size": 0.2,
+            "agg_function": "median",
+            "conformity_score": None,
+            "random_state": 42,
+        }
+
+        return [params1, params2]
