216a7685c3059a6f27034dbd26f60e81db5f5287
Franz Király <franzkiraly@inf.elte.hu>
2023-12-28 01:02:02 +0100

    [ENH] adapter from `scipy` `rv_discrete` to `skpro` `Empirical` (#155)
    This PR adds an adapter from lists of `scipy` `rv_discrete` to `skpro`
    `Empirical`.
    
    This adapter will be useful in interfacing `statsmodels` models that
    return lists of discrete `rv_discrete` distributions to represent
    tabular distribution objects.
    
    Also makes minor corrections to the `Empirical` docstring, and removes a
    stray line.

---
+++ skpro/distributions/adapters/__init__.py
@@ -0,0 +1,2 @@
+"""Adapters for probability distribution objects."""
+# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
---
+++ skpro/distributions/adapters/scipy/tests/__init__.py
@@ -0,0 +1,2 @@
+"""Tests for adapters for probability distribution objects, scipy facing."""
+# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
