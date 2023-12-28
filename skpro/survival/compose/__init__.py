0eb0b925c5d04ca415fe8c6fcde28c479f463ffb
Franz Király <f.kiraly@ucl.ac.uk>
2023-12-28 20:33:12 +0100

    fit uncensored

---
+++ skpro/survival/compose/__init__.py
@@ -0,0 +1,6 @@
+"""Survival or time-to-event prediction estimators, composers."""
+
+from skpro.regression.compose import Pipeline
+from skpro.survival.compose._reduce_uncensored import FitUncensored
+
+__all__ = ["Pipeline", "FitUncensored"]
