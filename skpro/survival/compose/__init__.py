949b743516b5e1e4de4597d7721f0c8cfe66f20b
Franz Király <f.kiraly@ucl.ac.uk>
2023-12-29 00:06:13 +0100

    Update __init__.py

--- skpro/survival/compose/__init__.py
+++ skpro/survival/compose/__init__.py
@@ -1,7 +1,7 @@
 """Survival or time-to-event prediction estimators, composers."""
 
 from skpro.regression.compose import Pipeline
-from skpro.survival.compose._reduce_cr_chain import ConditionUncensored
+from skpro.survival.compose._reduce_cond_unc import ConditionUncensored
 from skpro.survival.compose._reduce_uncensored import FitUncensored
 
 __all__ = [
