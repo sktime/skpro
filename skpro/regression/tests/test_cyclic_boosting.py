4ebcf433c1d5cb0adebc0bcd9e09794ab124b0e9
setoguchi-naoki <setoguchi.naoki@jp.panasonic.com>
2024-01-18 11:10:17 +0900

    add test for cyclic boosting with manual paramaters

--- skpro/regression/tests/test_cyclic_boosting.py
+++ skpro/regression/tests/test_cyclic_boosting.py
@@ -16,12 +16,33 @@ def test_cyclic_boosting_simple_use():
     from sklearn.datasets import load_diabetes
     from sklearn.model_selection import train_test_split
 
-    from skpro.regression.cyclic_boosting import CyclicBoosting
+    X, y = load_diabetes(return_X_y=True, as_frame=True)
+    y = pd.DataFrame(y)
+    X = X.iloc[:200]
+    y = y.iloc[:200]
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    reg_proba = CyclicBoosting()
+    reg_proba.fit(X_train, y_train)
+    y_pred = reg_proba.predict_proba(X_test)
+
+    assert y_pred.shape == y_test.shape
+
+
+@pytest.mark.skipif(
+    not run_test_for_class(CyclicBoosting),
+    reason="run test only if softdeps are present and incrementally (if requested)",
+)
+def test_cyclic_boosting_with_manual_paramaters():
+    """Test use of cyclic boosting regressor with_manual_paramaters."""
+    from cyclic_boosting import flags
+    from sklearn.datasets import load_diabetes
+    from sklearn.model_selection import train_test_split
 
     X, y = load_diabetes(return_X_y=True, as_frame=True)
     y = pd.DataFrame(y)
-    X = X.iloc[:50]
-    y = y.iloc[:50]
+    X = X.iloc[:200]
+    y = y.iloc[:200]
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
     features = [
@@ -38,7 +59,28 @@ def test_cyclic_boosting_simple_use():
         ("age", "sex"),
     ]
 
-    reg_proba = CyclicBoosting(feature_groups=features)
+    fp = {
+        "age": flags.IS_CONTINUOUS,
+        "sex": flags.IS_CONTINUOUS,
+        "bmi": flags.IS_CONTINUOUS,
+        "bp": flags.IS_CONTINUOUS,
+        "s1": flags.IS_CONTINUOUS,
+        "s2": flags.IS_CONTINUOUS,
+        "s3": flags.IS_CONTINUOUS,
+        "s4": flags.IS_CONTINUOUS,
+        "s5": flags.IS_CONTINUOUS,
+        "s6": flags.IS_CONTINUOUS,
+    }
+
+    reg_proba = CyclicBoosting(
+        feature_groups=features,
+        feature_properties=fp,
+        maximal_iterations=20,
+        alpha=0.25,
+        mode="additive",
+        bound="S",
+        lower=0.0,
+    )
     reg_proba.fit(X_train, y_train)
     y_pred = reg_proba.predict_proba(X_test)
 
