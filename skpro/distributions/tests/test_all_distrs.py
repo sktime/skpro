90357b908f3f35338cf73d3fcf45ae06694b25c2
Franz Király <f.kiraly@ucl.ac.uk>
2024-01-30 15:18:34 +0100

    Revert "[MNT] skip `CyclicBoosting` and QPD tests until #189 failures are resolved"

--- skpro/distributions/tests/test_all_distrs.py
+++ skpro/distributions/tests/test_all_distrs.py
@@ -60,11 +60,6 @@ METHODS_ROWWISE = ["energy"]  # results in one column
 class TestAllDistributions(PackageConfig, DistributionFixtureGenerator, QuickTester):
     """Module level tests for all skpro parameter fitters."""
 
-    # TEMPORARY skip for CyclicBoosting and QPD classes
-    # due to silent failures on main, se #190
-    exclude_objects = ["QPD_S", "QPD_B", "QPD_U"]
-    # remove this when fixing failures to re-enable testing
-
     @pytest.mark.parametrize("shuffled", [False, True])
     def test_sample(self, object_instance, shuffled):
         """Test sample expected return."""
