8baf010cb62b580f710db2e858309e5e028ec6b2
bhavikar04 <bhavikar04@gmail.com>
2024-03-15 14:08:05 +0530

    Remove log_normal.py test file

--- skpro/distributions/__init__.py
+++ skpro/distributions/__init__.py
@@ -3,8 +3,9 @@
 # copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
 # adapted from sktime
 
-__all__ = ["Empirical", "Laplace", "Normal"]
+__all__ = ["Log-Normal","Empirical", "Laplace", "Normal"]
 
 from skpro.distributions.empirical import Empirical
 from skpro.distributions.laplace import Laplace
 from skpro.distributions.normal import Normal
+from skpro.distributions.log_normal import Log_Normal
