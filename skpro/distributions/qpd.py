00fef17a90d091303dff719844864fbe1609b943
setoguchi-naoki <setoguchi.naoki@jp.panasonic.com>
2024-04-03 13:04:20 +0900

    minor change

--- skpro/distributions/qpd.py
+++ skpro/distributions/qpd.py
@@ -68,7 +68,7 @@ class QPD_S(BaseDistribution):
         # --------------
         "authors": ["setoguchi-naoki", "felix-wick"],
         "maintainers": ["setoguchi-naoki"],
-        "python_dependencies": "cyclic_boosting>=1.4.0; findiff",
+        "python_dependencies": ["cyclic_boosting>=1.4.0", "findiff"],
         # estimator tags
         # --------------
         "capabilities:approx": [],
@@ -273,7 +273,7 @@ class QPD_B(BaseDistribution):
         # --------------
         "authors": ["setoguchi-naoki", "felix-wick"],
         "maintainers": ["setoguchi-naoki"],
-        "python_dependencies": "cyclic_boosting>=1.2.5",
+        "python_dependencies": ["cyclic_boosting>=1.4.0", "findiff"],
         # estimator tags
         # --------------
         "capabilities:approx": [],
