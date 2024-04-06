96592cf95be6910a3f7608cf444bd6388dd8f09c
Franz Király <f.kiraly@ucl.ac.uk>
2024-04-06 22:09:42 +0100

    Update __init__.py

--- skpro/survival/coxph/__init__.py
+++ skpro/survival/coxph/__init__.py
@@ -1,6 +1,7 @@
 """Cox proportional hazards models."""
 
 from skpro.survival.coxph._coxnet_sksurv import CoxNet
+from skpro.survival.coxph._coxph_sksurv import CoxPHSkSurv
 from skpro.survival.coxph._coxph_statsmodels import CoxPH
 
-__all__ = ["CoxNet", "CoxPH"]
+__all__ = ["CoxNet", "CoxPH", "CoxPHSkSurv"]
