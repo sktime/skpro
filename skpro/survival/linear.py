a16ba233162128da7c44ed3a52f0ab5df6ae647e
Franz Király <f.kiraly@ucl.ac.uk>
2024-04-06 14:33:27 +0100

    Update linear.py

--- skpro/survival/linear.py
+++ skpro/survival/linear.py
@@ -8,7 +8,7 @@ from skpro.survival.base import BaseSurvReg
 
 
 class CoxNet(_SksurvAdapter, BaseSurvReg):
-    """Cox’s proportional hazard’s model with elastic net penalty.
+    """Cox proportional hazard model with elastic net penalty.
 
     Direct interface to ``sksurv.linear_model.CoxnetSurvivalAnalysis``, by ``sebp``.
 
