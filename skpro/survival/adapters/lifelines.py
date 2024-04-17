533599989c333a3eaa0d2fbf838d9dd7ba78099f
Franz Király <f.kiraly@ucl.ac.uk>
2024-04-17 12:12:57 +0100

    fix improper surv functions via clipping

--- skpro/survival/adapters/lifelines.py
+++ skpro/survival/adapters/lifelines.py
@@ -4,6 +4,8 @@
 __all__ = ["_LifelinesAdapter"]
 __author__ = ["fkiraly"]
 
+from warnings import warn
+
 import numpy as np
 import pandas as pd
 
@@ -142,7 +144,20 @@ class _LifelinesAdapter:
         times_df = pd.DataFrame(times_val, index=mi, columns=self._y_cols)
 
         lifelines_survf_t = np.transpose(lifelines_survf.values)
-        weights = -np.diff(lifelines_survf_t, axis=1).flatten()
+        _, lifelines_survf_t_diff, clipped = _clip_surv(lifelines_survf_t)
+
+        if clipped:
+            warn(
+                f"Warning from {self.__class__.__name__}: "
+                f"Interfaced lifelines class {lifelines_est.__class__.__name__} "
+                "produced improper survival function predictions, i.e., "
+                "not monotonically decreasing or not in [0, 1]. "
+                "skpro has clipped the predictions to enforce proper range and "
+                "valid predictive distributions. "
+                "However, predictions may still be unreliable."
+            )
+
+        weights = -lifelines_survf_t_diff.flatten()
         weights_df = pd.Series(weights, index=mi)
 
         dist = Empirical(
@@ -150,3 +165,53 @@ class _LifelinesAdapter:
         )
 
         return dist
+
+
+def _clip_surv(surv_arr):
+    """Clips improper survival function values to proper range.
+
+    Enforces: values are in [0, 1] and are monotonically decreasing.
+
+    First clips to [0, 1], then enforces monotonicity, by replacing
+    any value with minimum of itself and any previous values.
+
+    Parameters
+    ----------
+    surv_arr : 2D np.ndarray
+        Survival function values.
+        index 0 is instance index.
+        index 1 is time index, increasing.
+
+    Returns
+    -------
+    surv_arr_clipped : 2D np.ndarray
+        Clipped survival function values.
+    surv_arr_diff : 2D np.ndarray
+        Difference of clipped survival function values.
+        Same as np.diff(surv_arr_clipped, axis=1).
+        Returned to avoid recomputation, if needed later in context.
+    clipped : boolean
+        Whether clipping was needed.
+    """
+    too_large = surv_arr > 1
+    too_small = surv_arr < 0
+
+    surv_arr[too_large] = 1
+    surv_arr[too_small] = 0
+
+    surv_arr_diff = np.diff(surv_arr, axis=1)
+
+    # avoid iterative minimization if no further clipping is needed
+    if np.sum(surv_arr_diff == 0):
+        clipped = too_large.any() or too_small.any()
+        return surv_arr, surv_arr_diff, clipped
+
+    # enforce monotonicity
+    # iterating from left to right ensures values are replaced
+    # with minimum of itself and all values to the left
+    for i in range(1, surv_arr.shape[1]):
+        surv_arr[:, i] = np.minimum(surv_arr[:, i], surv_arr[:, i - 1])
+
+    surv_arr_diff = np.diff(surv_arr, axis=1)
+
+    return surv_arr, surv_arr_diff, True
