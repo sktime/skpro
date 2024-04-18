72b3c18b687ab4c676824dd6311e48e6a6946798
Franz Király <f.kiraly@ucl.ac.uk>
2024-04-18 19:11:54 +0100

    Update qpd.py

--- skpro/distributions/qpd.py
+++ skpro/distributions/qpd.py
@@ -15,7 +15,7 @@ from scipy.integrate import quad
 from scipy.misc import derivative
 from scipy.stats import logistic, norm
 
-from skpro.distributions.base import _DelegatedDistribution, BaseDistribution
+from skpro.distributions.base import BaseDistribution, _DelegatedDistribution
 
 
 class QPD_Johnson(_DelegatedDistribution):
