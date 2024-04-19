369cee7b5fb6f4470d7a6129ee4a667651970c95
Franz Király <f.kiraly@ucl.ac.uk>
2024-04-19 16:32:17 +0100

    [ENH] log-normal AFT model from `lifelines` (#258)
    Adds the log-normal AFT model from `lifelines`.
    
    Towards #250

--- skpro/survival/aft/__init__.py
+++ skpro/survival/aft/__init__.py
@@ -1,6 +1,7 @@
 """Module containing accelerated failure time models."""
 # copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
 
-__all__ = ["AFTWeibull"]
+__all__ = ["AFTLogNormal", "AFTWeibull"]
 
+from skpro.survival.aft._aft_lifelines_lognormal import AFTLogNormal
 from skpro.survival.aft._aft_lifelines_weibull import AFTWeibull
