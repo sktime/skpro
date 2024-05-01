8eb606537d3c45c2300fcd3301e622c453d9ab49
malikrafsan <pro.malikakbar2357@gmail.com>
2024-05-01 17:38:23 +0700

    chore: fix codes on formatting pre-commit

--- skpro/distributions/adapters/scipy/__init__.py
+++ skpro/distributions/adapters/scipy/__init__.py
@@ -1,7 +1,10 @@
 """Adapters for probability distribution objects, scipy facing."""
 # copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
 
+from skpro.distributions.adapters.scipy._distribution import (
+    _ScipyAdapter,
+    _ScipyDiscreteAdapter,
+)
 from skpro.distributions.adapters.scipy._empirical import empirical_from_discrete
-from skpro.distributions.adapters.scipy._distribution import _ScipyAdapter, _ScipyDiscreteAdapter
 
 __all__ = ["empirical_from_discrete", "_ScipyAdapter", "_ScipyDiscreteAdapter"]
