d09e3e2582a2df7e9e94aec5c5915b0049059d4f
Franz Király <f.kiraly@ucl.ac.uk>
2024-05-04 16:10:41 +0100

    Update _config.py

--- skpro/tests/_config.py
+++ skpro/tests/_config.py
@@ -2,7 +2,6 @@
 
 from skpro.tests._config_test_dummy import DummySkipped
 
-
 # list of str, names of estimators to exclude from testing
 # WARNING: tests for these estimators will be skipped
 EXCLUDE_ESTIMATORS = [DummySkipped]
