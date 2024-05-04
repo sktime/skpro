0a1ec49f1a691be8a40409240e6c85c5d9955558
Franz Király <f.kiraly@ucl.ac.uk>
2024-05-04 11:33:54 +0100

    test for test_switch

--- skpro/tests/_config.py
+++ skpro/tests/_config.py
@@ -3,3 +3,11 @@
 # list of str, names of estimators to exclude from testing
 # WARNING: tests for these estimators will be skipped
 EXCLUDE_ESTIMATORS = []
+
+
+from skpro.regression.base import BaseProbaRegressor  # noqa: E402
+
+
+class DummySkipped(BaseProbaRegressor):
+    """Dummy regressor to test exclusion."""
+    pass
