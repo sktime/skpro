075166db281020ee58dec550d1d00aa84138ad06
ShreeshaM07 <shreesham2k22@yahoo.com>
2024-05-07 17:07:01 +0530

    [ENH] Interface NGBSurvival in skpro

---
+++ skpro/survival/gradient_boosting_surv/__init__.py
@@ -0,0 +1,6 @@
+"""Natural Gradient Boosting Regressor models."""
+# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
+
+from skpro.survival.gradient_boosting_surv._ngboost_surv import NGBoostSurvival
+
+__all__ = ["NGBoostSurvival"]
