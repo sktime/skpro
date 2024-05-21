9ccbcacd7c725b0a949ef93cfd4f16b7f0ab60ea
ShreeshaM07 <shreesham2k22@yahoo.com>
2024-05-22 02:02:41 +0530

    energy when x is outside the possible X

--- skpro/distributions/histogram.py
+++ skpro/distributions/histogram.py
@@ -30,11 +30,12 @@ class Histogram(BaseDistribution):
     """
 
     _tags = {
-        #     "capabilities:approx": ["pdfnorm"],
-        "capabilities:exact": ["mean", "var", "energy", "pdf", "log_pdf", "cdf", "ppf"],
-        #     "distr:measuretype": "continuous",
-        #     "distr:paramtype": "parametric",
-        #     "broadcast_init": "on",
+        "authors": ["ShreeshaM07"],
+        "capabilities:approx": ["pdfnorm", "energy"],
+        "capabilities:exact": ["mean", "var", "pdf", "log_pdf", "cdf", "ppf"],
+        "distr:measuretype": "continuous",
+        "distr:paramtype": "parametric",
+        # "broadcast_init": "on",
     }
 
     def __init__(self, bins, bin_mass, index=None, columns=None):
@@ -42,6 +43,72 @@ class Histogram(BaseDistribution):
         self.bin_mass = bin_mass
 
         super().__init__(index=index, columns=columns)
+
+    def _convert_tuple_to_array(self, bins):
+        bins_to_list = (bins[0], bins[1], bins[2])
+        bins = []
+        bin_width = (bins_to_list[1] - bins_to_list[0]) / bins_to_list[2]
+        for b in range(bins_to_list[2]):
+            bins.append(bins_to_list[0] + b * bin_width)
+        bins.append(bins_to_list[1])
+        return bins
+
+    def _energy_self(self):
+        r"""Energy of self, w.r.t. self.
+
+        :math:`\mathbb{E}[|X-Y|]`, where :math:`X, Y` are i.i.d. copies of self.
+
+        Private method, to be implemented by subclasses.
+
+        Returns
+        -------
+        2D np.ndarray, same shape as ``self``
+            energy values w.r.t. the given points
+        """
+        bins = self.bins
+        # bin_mass = self.bin_mass
+        # convert the bins into a list
+        if isinstance(bins, tuple):
+            bins = self._convert_tuple_to_array(bins)
+
+    def _energy_x(self, x):
+        r"""Energy of self, w.r.t. a constant frame x.
+
+        :math:`\mathbb{E}[|X-x|]`, where :math:`X` is a copy of self,
+        and :math:`x` is a constant.
+
+        Private method, to be implemented by subclasses.
+
+        Parameters
+        ----------
+        x : 1D np.ndarray, same shape as ``self``
+            values to compute energy w.r.t. to
+
+        Returns
+        -------
+        1D np.ndarray, same shape as ``self``
+            energy values w.r.t. the given points
+        """
+        bins = self.bins
+        bin_mass = self.bin_mass
+        energy_arr = []
+        # convert the bins into a list
+        if isinstance(bins, tuple):
+            bins = self._convert_tuple_to_array(bins)
+
+        if isinstance(bins, list):
+            mean = self._mean()
+
+            is_outside = np.logical_or(x < bins[0], x > bins[-1])
+            # is_inside = 1 - is_outside
+
+            if is_outside:
+                energy_arr = abs(mean - x * bin_mass)
+            # else:
+            #     bin_idx_pre_x = np.where(x >= bins)[0][-1]
+            #     still in progress ...
+
+            return energy_arr
 
     def _mean(self):
         """Return expected value of the distribution.
@@ -55,12 +122,7 @@ class Histogram(BaseDistribution):
         bin_mass = np.array(self.bin_mass)
         # convert the bins into a list
         if isinstance(bins, tuple):
-            bins_to_list = (bins[0], bins[1], bins[2])
-            bins = []
-            bin_width = (bins_to_list[1] - bins_to_list[0]) / bins_to_list[2]
-            for b in range(bins_to_list[2]):
-                bins.append(bins_to_list[0] + b * bin_width)
-            bins.append(bins_to_list[1])
+            bins = self._convert_tuple_to_array(bins)
 
         if isinstance(bins, list):
             from numpy.lib.stride_tricks import sliding_window_view
@@ -82,15 +144,9 @@ class Histogram(BaseDistribution):
 
         # convert the bins into a list
         if isinstance(bins, tuple):
-            bins_to_list = (bins[0], bins[1], bins[2])
-            bins = []
-            bin_width = (bins_to_list[1] - bins_to_list[0]) / bins_to_list[2]
-            for b in range(bins_to_list[2]):
-                bins.append(bins_to_list[0] + b * bin_width)
-            bins.append(bins_to_list[1])
+            bins = self._convert_tuple_to_array(bins)
 
         if isinstance(bins, list):
-            bin_width = np.diff(bins)
             from numpy.lib.stride_tricks import sliding_window_view
 
             win_sum_bins = np.sum(sliding_window_view(bins, window_shape=2), axis=1)
@@ -118,12 +174,7 @@ class Histogram(BaseDistribution):
 
         # convert the bins into a list
         if isinstance(bins, tuple):
-            bins_to_list = (bins[0], bins[1], bins[2])
-            bins = []
-            bin_width = (bins_to_list[1] - bins_to_list[0]) / bins_to_list[2]
-            for b in range(bins_to_list[2]):
-                bins.append(bins_to_list[0] + b * bin_width)
-            bins.append(bins_to_list[1])
+            bins = self._convert_tuple_to_array(bins)
 
         if isinstance(bins, list):
             bin_width = np.diff(bins)
@@ -135,6 +186,38 @@ class Histogram(BaseDistribution):
                     pdf.append(0)
             pdf = np.array(pdf)
             return pdf
+
+    def _log_pdf(self, x):
+        """Logarithmic probability density function.
+
+        Parameters
+        ----------
+        x : 2D np.ndarray, same shape as ``self``
+            values to evaluate the pdf at
+
+        Returns
+        -------
+        2D np.ndarray, same shape as ``self``
+            log pdf values at the given points
+        """
+        bin_mass = np.array(self.bin_mass.copy())
+        bins = self.bins
+        lpdf = []
+
+        # convert the bins into a list
+        if isinstance(bins, tuple):
+            bins = self._convert_tuple_to_array(bins)
+
+        if isinstance(bins, list):
+            bin_width = np.diff(bins)
+            lpdf_arr = np.log(bin_mass / bin_width)
+            for X in x:
+                if len(np.where(X < bins)[0]) and len(np.where(X >= bins)[0]):
+                    lpdf.append(lpdf_arr[min(np.where(X < bins)[0]) - 1])
+                else:
+                    lpdf.append(0)
+            lpdf = np.array(lpdf)
+            return lpdf
 
     def _cdf(self, x):
         """Cumulative distribution function.
@@ -156,12 +239,7 @@ class Histogram(BaseDistribution):
 
         # convert the bins into a list
         if isinstance(bins, tuple):
-            bins_to_list = (bins[0], bins[1], bins[2])
-            bins = []
-            bin_width = (bins_to_list[1] - bins_to_list[0]) / bins_to_list[2]
-            for b in range(bins_to_list[2]):
-                bins.append(bins_to_list[0] + b * bin_width)
-            bins.append(bins_to_list[1])
+            bins = self._convert_tuple_to_array(bins)
 
         if isinstance(bins, list):
             cum_sum_mass = np.cumsum(bin_mass)
@@ -203,12 +281,7 @@ class Histogram(BaseDistribution):
 
         # convert the bins into a list
         if isinstance(bins, tuple):
-            bins_to_list = (bins[0], bins[1], bins[2])
-            bins = []
-            bin_width = (bins_to_list[1] - bins_to_list[0]) / bins_to_list[2]
-            for b in range(bins_to_list[2]):
-                bins.append(bins_to_list[0] + b * bin_width)
-            bins.append(bins_to_list[1])
+            bins = self._convert_tuple_to_array(bins)
 
         if isinstance(bins, list):
             cum_sum_mass = np.cumsum(bin_mass)
