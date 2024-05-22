5548626127c813763b461cd54df4ab5d83536022
Franz Király <f.kiraly@ucl.ac.uk>
2024-05-22 02:07:29 +0100

    Update test_empirical.py

--- skpro/distributions/tests/test_empirical.py
+++ skpro/distributions/tests/test_empirical.py
@@ -7,9 +7,7 @@ from skpro.distributions.empirical import Empirical
 
 def test_empirical_iat_index():
     """Test that the index is correctly set after iat call."""
-    spl_idx = pd.MultiIndex.from_product(
-        [[0, 1], [0, 1, 2]], names=["sample", "time"]
-    )
+    spl_idx = pd.MultiIndex.from_product([[0, 1], [0, 1, 2]], names=["sample", "time"])
     spl = pd.DataFrame(
         [[0, 1], [2, 3], [10, 11], [6, 7], [8, 9], [4, 5]],
         index=spl_idx,
