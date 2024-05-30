fe5333bf923b61ba2564d1c044a3e40e74df26b9
julian fong <cyfongj@gmail.com>
2024-05-29 23:08:53 -0400

    initial commit

--- skpro/tests/test_polars.py
+++ skpro/tests/test_polars.py
@@ -1,6 +1,21 @@
 """Test file for polars dataframes"""
 
-# import pandas as pd
-# import pytest
+import pytest
 
-# from skpro.utils.validation._dependencies import _check_soft_dependencies
+# from skpro.datatypes._table._convert import (
+#     convert_polars_to_pandas,
+#     convert_pandas_to_polars_eager
+# )
+from sktime.utils.validation._dependencies import _check_soft_dependencies
+
+
+@pytest.mark.skipif(
+    not _check_soft_dependencies("polars", severity="none"),
+    reason="skip test if polars is not installed in environment",
+)
+def test_polars_dataframe_in_fit():
+    pass
+
+
+def test_polars_dataframe_in_predict():
+    pass
