0312baf6b9a9700a285755de742a0763ffcfcf1f
Sukrit Jindal <132339317+sukjingitsit@users.noreply.github.com>
2024-06-20 11:21:13 +0530

    Update pareto.py

--- skpro/distributions/pareto.py
+++ skpro/distributions/pareto.py
@@ -12,13 +12,13 @@ from skpro.distributions.base import BaseDistribution
 class Pareto(BaseDistribution):
     r"""Pareto distribution (skpro native).
 
-    The scale :math:`x_m` is represented by the parameter ``xm``,
+    The scale is represented by the parameter ``scale``,
     and the Pareto index (or shape parameter) :math:`\alpha`
     by the parameter ``alpha``.
 
     Parameters
     ----------
-    xm : float or array of float (1D or 2D), must be positive
+    scale : float or array of float (1D or 2D), must be positive
         scale of the Pareto distribution
     alpha : float or array of float (1D or 2D), must be positive
         shape of the Pareto distribution
@@ -29,7 +29,7 @@ class Pareto(BaseDistribution):
     -------
     >>> from skpro.distributions.pareto import Pareto
 
-    >>> n = Pareto(xm=[[1, 1.5], [2, 2.5], [3, 4]], alpha=3)
+    >>> n = Pareto(scale=[[1, 1.5], [2, 2.5], [3, 4]], alpha=3)
     """
 
     _tags = {
@@ -40,8 +40,8 @@ class Pareto(BaseDistribution):
         "broadcast_init": "on",
     }
 
-    def __init__(self, xm, alpha, index=None, columns=None):
-        self.xm = xm
+    def __init__(self, scale, alpha, index=None, columns=None):
+        self.scale = scale
         self.alpha = alpha
 
         super().__init__(index=index, columns=columns)
@@ -55,8 +55,8 @@ class Pareto(BaseDistribution):
             expected value of distribution (entry-wise)
         """
         alpha = self._bc_params["alpha"]
-        xm = self._bc_params["xm"]
-        mean = np.where(alpha <= 1, np.infty, xm**alpha / (alpha - 1))
+        scale = self._bc_params["scale"]
+        mean = np.where(alpha <= 1, np.infty, scale**alpha / (alpha - 1))
         return mean
 
     def _var(self):
@@ -68,9 +68,9 @@ class Pareto(BaseDistribution):
             variance of the distribution (entry-wise)
         """
         alpha = self._bc_params["alpha"]
-        xm = self._bc_params["xm"]
+        scale = self._bc_params["scale"]
         var = np.where(
-            alpha <= 2, np.infty, xm**2 * alpha / ((alpha - 2) * (alpha - 1) ** 2)
+            alpha <= 2, np.infty, scale**2 * alpha / ((alpha - 2) * (alpha - 1) ** 2)
         )
         return var
 
@@ -88,8 +88,8 @@ class Pareto(BaseDistribution):
             pdf values at the given points
         """
         alpha = self._bc_params["alpha"]
-        xm = self._bc_params["xm"]
-        pdf_arr = alpha * np.power(xm, alpha)
+        scale = self._bc_params["scale"]
+        pdf_arr = alpha * np.power(scale, alpha)
         pdf_arr /= np.power(x, alpha + 1)
         return pdf_arr
 
@@ -107,8 +107,8 @@ class Pareto(BaseDistribution):
             log pdf values at the given points
         """
         alpha = self._bc_params["alpha"]
-        xm = self._bc_params["xm"]
-        return np.log(alpha / x) + alpha * np.log(xm / x)
+        scale = self._bc_params["scale"]
+        return np.log(alpha / x) + alpha * np.log(scale / x)
 
     def _cdf(self, x):
         """Cumulative distribution function.
@@ -124,8 +124,8 @@ class Pareto(BaseDistribution):
             cdf values at the given points
         """
         alpha = self._bc_params["alpha"]
-        xm = self._bc_params["xm"]
-        cdf_arr = np.where(x < xm, 0, 1 - np.power(xm / x, alpha))
+        scale = self._bc_params["scale"]
+        cdf_arr = np.where(x < scale, 0, 1 - np.power(scale / x, alpha))
         return cdf_arr
 
     def _ppf(self, p):
@@ -142,20 +142,20 @@ class Pareto(BaseDistribution):
             ppf values at the given points
         """
         alpha = self._bc_params["alpha"]
-        xm = self._bc_params["xm"]
-        return xm / np.power(1 - p, 1 / alpha)
+        scale = self._bc_params["scale"]
+        return scale / np.power(1 - p, 1 / alpha)
 
     @classmethod
     def get_test_params(cls, parameter_set="default"):
         """Return testing parameter settings for the estimator."""
         # array case examples
-        params1 = {"xm": [[1, 1.5], [2, 3], [4, 5]], "alpha": 3}
+        params1 = {"scale": [[1, 1.5], [2, 3], [4, 5]], "alpha": 3}
         params2 = {
-            "xm": 1,
+            "scale": 1,
             "alpha": 3,
             "index": pd.Index([1, 2, 5]),
             "columns": pd.Index(["a", "b"]),
         }
         # scalar case examples
-        params3 = {"xm": 1, "alpha": 2}
+        params3 = {"scale": 1, "alpha": 2}
         return [params1, params2, params3]
