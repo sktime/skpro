2e16e9d7def6f47a2c3484010b3ce4d356bb85d1
ShreeshaM07 <shreesham2k22@yahoo.com>
2024-06-27 14:25:50 +0530

    get_test_params

--- skpro/classification/sklearn_classifiers/_sklearn_proba_class.py
+++ skpro/classification/sklearn_classifiers/_sklearn_proba_class.py
@@ -189,3 +189,29 @@ class SklearnProbaClassifier(BaseProbaRegressor):
             bins=bins, bin_mass=bin_mass, index=X.index, columns=self._y_cols
         )
         return pred_proba
+
+    @classmethod
+    def get_test_params(cls, parameter_set="default"):
+        """Return testing parameter settings for the estimator.
+
+        Parameters
+        ----------
+        parameter_set : str, default="default"
+            Name of the set of test parameters to return, for use in tests. If no
+            special parameters are defined for a value, will return `"default"` set.
+
+        Returns
+        -------
+        params : dict or list of dict, default = {}
+            Parameters to create testing instances of the class
+            Each dict are parameters to construct an "interesting" test instance, i.e.,
+            `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
+            `create_test_instance` uses the first (or only) dictionary in `params`
+        """
+        from sklearn.naive_bayes import GaussianNB
+        from sklearn.tree import DecisionTreeClassifier
+
+        param1 = {"clf": DecisionTreeClassifier(), "bins": 4}
+        param2 = {"clf": GaussianNB()}
+
+        return [param1, param2]
