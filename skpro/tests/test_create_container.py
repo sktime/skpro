78e3041f4296315ba8b0406438fd7889e2c19e2b
julian fong <cyfongj@gmail.com>
2024-07-01 12:44:03 -0400

    updates

--- skpro/tests/test_create_container.py
+++ skpro/tests/test_create_container.py
@@ -1,8 +1,14 @@
 # test file for create_container methods
-# import pandas as pd
+import pandas as pd
+
 # import pytest
 
 # from skpro.utils.validation._dependencies import _check_soft_dependencies
 
 # if _check_soft_dependencies(["polars", "pyarrow"], severity="none"):
 #     import polars as pl
+
+# test following cases, numpy input, polars input, pandas input
+
+data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+df = pd.DataFrame(data)
