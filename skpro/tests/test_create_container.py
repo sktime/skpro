b335de6ce5e9978002796e1a0cb33c3c1d0423c7
julian fong <cyfongj@gmail.com>
2024-07-02 18:03:19 -0400

    updated test container tests

--- skpro/tests/test_create_container.py
+++ skpro/tests/test_create_container.py
@@ -5,20 +5,20 @@ import pytest
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 
-# from skpro.utils.create_container import PandasAdapter, get_config_adapter
-# from skpro.utils.set_output import check_transform_config
-# from skpro.utils.validation._dependencies import _check_soft_dependencies
+from skpro.utils.create_container import PandasAdapter, get_config_adapter
+from skpro.utils.set_output import check_transform_config
+from skpro.utils.validation._dependencies import _check_soft_dependencies
 
-# if _check_soft_dependencies(["polars", "pyarrow"], severity="none"):
-#     from skpro.utils.create_container import PolarsAdapter
+if _check_soft_dependencies(["polars", "pyarrow"], severity="none"):
+    from skpro.utils.create_container import PolarsAdapter
 
 
 # test following cases, numpy input, polars input, pandas input
 @pytest.fixture
 def load_diabetes_pandas():
     X, y = load_diabetes(return_X_y=True, as_frame=True)
-    X = X.iloc[:15]
-    y = y.iloc[:15]
+    X = X.iloc[:75]
+    y = y.iloc[:75]
 
     # typically y is returned as a pd.Series to we call y as a Dataframe here
     y = pd.DataFrame(y)
@@ -88,119 +88,121 @@ def estimator():
     return _estimator
 
 
-# def test_get_config_adapter_pandas(estimator, load_diabetes_pandas):
-#     estimator.set_output(transform="pandas")
-#     X_train, X_test, y_train = load_diabetes_pandas
+def test_get_config_adapter_pandas(estimator, load_diabetes_pandas):
+    estimator.set_output(transform="pandas")
+    X_train, X_test, y_train = load_diabetes_pandas
 
-#     estimator.fit(X_train, y_train)
-#     y_pred_q = estimator.predict_quantiles(X_test)[:5]
-#     y_pred_i = estimator.predict_interval(X_test)[:5]
-#     y_pred_v = estimator.predict_var(X_test)[:5]
+    estimator.fit(X_train, y_train)
+    y_pred_q = estimator.predict_quantiles(X_test)[:5]
+    y_pred_i = estimator.predict_interval(X_test)[:5]
+    y_pred_v = estimator.predict_var(X_test)[:5]
 
-#     _, output_config = check_transform_config(estimator)
-#     transform_adapter = output_config["dense"]
+    _, output_config = check_transform_config(estimator)
+    transform_adapter = output_config["dense"]
 
-#     adapter, columns = get_config_adapter(transform_adapter, y_pred_q)
-#     assert isinstance(adapter, PandasAdapter)
-#     assert (columns == y_pred_q.columns).all()
+    adapter, columns = get_config_adapter(transform_adapter, y_pred_q)
+    assert isinstance(adapter, PandasAdapter)
+    assert (columns == y_pred_q.columns).all()
 
-#     adapter, columns = get_config_adapter(transform_adapter, y_pred_i)
-#     assert isinstance(adapter, PandasAdapter)
-#     assert (columns == y_pred_i.columns).all()
+    adapter, columns = get_config_adapter(transform_adapter, y_pred_i)
+    assert isinstance(adapter, PandasAdapter)
+    assert (columns == y_pred_i.columns).all()
 
-#     adapter, columns = get_config_adapter(transform_adapter, y_pred_v)
-#     assert isinstance(adapter, PandasAdapter)
-#     assert (columns == y_pred_v.columns).all()
-
-
-# @pytest.mark.skipif(
-#     not _check_soft_dependencies(["polars", "pyarrow"], severity="none"),
-#     reason="skip test if polars/pyarrow is not installed in environment",
-# )
-# def test_get_config_adapter_polars(estimator, load_diabetes_pandas):
-#     estimator.set_output(transform="polars")
-#     X_train, X_test, y_train = load_diabetes_pandas
-
-#     estimator.fit(X_train, y_train)
-#     y_pred_q = estimator.predict_quantiles(X_test)[:5]
-#     y_pred_i = estimator.predict_interval(X_test)[:5]
-#     y_pred_v = estimator.predict_var(X_test)[:5]
-
-#     _, output_config = check_transform_config(estimator)
-#     transform_adapter = output_config["dense"]
-
-#     adapter, columns = get_config_adapter(transform_adapter, y_pred_q)
-#     assert isinstance(adapter, PolarsAdapter)
-#     assert columns == ["__target__0.05__", "__target__0.95__"]
-
-#     adapter, columns = get_config_adapter(transform_adapter, y_pred_i)
-#     assert isinstance(adapter, PolarsAdapter)
-#     assert columns == ["__target__0.9__lower__", "__target__0.9__upper__"]
-
-#     adapter, columns = get_config_adapter(transform_adapter, y_pred_v)
-#     assert isinstance(adapter, PolarsAdapter)
-#     assert columns == ["target"]
+    adapter, columns = get_config_adapter(transform_adapter, y_pred_v)
+    assert isinstance(adapter, PandasAdapter)
+    assert (columns == y_pred_v.columns).all()
 
 
-# def test_create_container_to_pandas(estimator, load_diabetes_pandas):
-#     estimator.set_output(transform="pandas")
-#     X_train, X_test, y_train = load_diabetes_pandas
+@pytest.mark.skipif(
+    not _check_soft_dependencies(["polars", "pyarrow"], severity="none"),
+    reason="skip test if polars/pyarrow is not installed in environment",
+)
+def test_get_config_adapter_polars(estimator, load_diabetes_pandas):
+    estimator.set_output(transform="polars")
+    X_train, X_test, y_train = load_diabetes_pandas
 
-#     estimator.fit(X_train, y_train)
-#     y_pred_q = estimator.predict_quantiles(X_test)[:5]
-#     y_pred_i = estimator.predict_interval(X_test)[:5]
-#     y_pred_v = estimator.predict_var(X_test)[:5]
+    estimator.fit(X_train, y_train)
+    y_pred_q = estimator.predict_quantiles(X_test)[:5]
+    y_pred_i = estimator.predict_interval(X_test)[:5]
+    y_pred_v = estimator.predict_var(X_test)[:5]
 
-#     _, output_config = check_transform_config(estimator)
-#     transform_adapter = output_config["dense"]
+    _, output_config = check_transform_config(estimator)
+    transform_adapter = output_config["dense"]
 
-#     adapter, columns = get_config_adapter(transform_adapter, y_pred_q)
-#     y_pred_q_out = adapter.create_container(y_pred_q, columns)
-#     assert (y_pred_q_out.values == y_pred_q.values).all()
-#     assert (y_pred_q_out.columns == y_pred_q.columns).all()
-#     assert (y_pred_q_out.index == y_pred_q.index).all()
+    adapter, columns = get_config_adapter(transform_adapter, y_pred_q)
+    assert isinstance(adapter, PolarsAdapter)
+    assert columns == ["__target__0.05__", "__target__0.95__"]
 
-#     adapter, columns = get_config_adapter(transform_adapter, y_pred_i)
-#     y_pred_i_out = adapter.create_container(y_pred_q, columns)
-#     assert (y_pred_i_out.values == y_pred_i.values).all()
-#     assert (y_pred_i_out.columns == y_pred_i.columns).all()
-#     assert (y_pred_i_out.index == y_pred_i.index).all()
+    adapter, columns = get_config_adapter(transform_adapter, y_pred_i)
+    assert isinstance(adapter, PolarsAdapter)
+    assert columns == ["__target__0.9__lower__", "__target__0.9__upper__"]
 
-#     adapter, columns = get_config_adapter(transform_adapter, y_pred_v)
-#     y_pred_v_out = adapter.create_container(y_pred_v, columns)
-#     assert (y_pred_v_out.values == y_pred_v.values).all()
-#     assert (y_pred_v_out.columns == y_pred_v.columns).all()
-#     assert (y_pred_v_out.index == y_pred_v.index).all()
+    adapter, columns = get_config_adapter(transform_adapter, y_pred_v)
+    assert isinstance(adapter, PolarsAdapter)
+    assert columns == ["target"]
 
 
-# @pytest.mark.skipif(
-#     not _check_soft_dependencies(["polars", "pyarrow"], severity="none"),
-#     reason="skip test if polars/pyarrow is not installed in environment",
-# )
-# def test_create_container_to_polars(estimator, load_diabetes_pandas):
-#     estimator.set_output(transform="polars")
-#     X_train, X_test, y_train = load_diabetes_pandas
+def test_create_container_to_pandas(
+    estimator,
+    load_diabetes_pandas,
+    load_pandas_pred_quantile,
+    load_pandas_pred_interval,
+    load_pandas_pred_var,
+):
+    estimator.set_output(transform="pandas")
+    expected_y_pred_int = load_pandas_pred_interval
+    expected_y_pred_quantile = load_pandas_pred_quantile
+    expected_y_pred_var = load_pandas_pred_var
+    X_train, X_test, y_train = load_diabetes_pandas
 
-#     estimator.fit(X_train, y_train)
-#     y_pred_q = estimator.predict_quantiles(X_test)[:5]
-#     y_pred_i = estimator.predict_interval(X_test)[:5]
-#     y_pred_v = estimator.predict_var(X_test)[:5]
+    estimator.fit(X_train, y_train)
+    y_pred_q = estimator.predict_quantiles(X_test)[:5]
+    y_pred_i = estimator.predict_interval(X_test)[:5]
+    y_pred_v = estimator.predict_var(X_test)[:5]
 
-#     _, output_config = check_transform_config(estimator)
-#     transform_adapter = output_config["dense"]
+    assert (expected_y_pred_quantile.values == y_pred_q.values).all()
+    assert (expected_y_pred_quantile.columns == y_pred_q.columns).all()
+    assert (expected_y_pred_quantile.index == y_pred_q.index).all()
 
-#     adapter, columns = get_config_adapter(transform_adapter, y_pred_q)
-#     y_pred_q_out = adapter.create_container(y_pred_q, columns)
-#     assert (y_pred_q_out.to_numpy() == y_pred_q.values).all()
-#     assert y_pred_q_out.columns == ["__target__0.05__", "__target__0.95__"]
+    assert (expected_y_pred_int.values == y_pred_i.values).all()
+    assert (expected_y_pred_int.columns == y_pred_i.columns).all()
+    assert (expected_y_pred_int.index == y_pred_i.index).all()
 
-#     adapter, columns = get_config_adapter(transform_adapter, y_pred_i)
-#     y_pred_i_out = adapter.create_container(y_pred_q, columns)
-#     assert (y_pred_i_out.to_numpy() == y_pred_i.values).all()
-#     assert y_pred_i_out.columns ==
-# ["__target__0.9__lower__", "__target__0.9__upper__"]
+    assert (expected_y_pred_var.values == y_pred_v.values).all()
+    assert (expected_y_pred_var.columns == y_pred_v.columns).all()
+    assert (expected_y_pred_var.index == y_pred_v.index).all()
 
-#     adapter, columns = get_config_adapter(transform_adapter, y_pred_v)
-#     y_pred_v_out = adapter.create_container(y_pred_v, columns)
-#     assert (y_pred_v_out.to_numpy() == y_pred_v.values).all()
-#     assert y_pred_v_out.columns == ["target"]
+
+@pytest.mark.skipif(
+    not _check_soft_dependencies(["polars", "pyarrow"], severity="none"),
+    reason="skip test if polars/pyarrow is not installed in environment",
+)
+def test_create_container_to_polars(
+    estimator,
+    load_diabetes_pandas,
+    load_pandas_pred_quantile,
+    load_pandas_pred_interval,
+    load_pandas_pred_var,
+):
+    estimator.set_output(transform="polars")
+    expected_y_pred_int = load_pandas_pred_interval
+    expected_y_pred_quantile = load_pandas_pred_quantile
+    expected_y_pred_var = load_pandas_pred_var
+    X_train, X_test, y_train = load_diabetes_pandas
+
+    estimator.fit(X_train, y_train)
+    y_pred_q = estimator.predict_quantiles(X_test)[:5]
+    y_pred_i = estimator.predict_interval(X_test)[:5]
+    y_pred_v = estimator.predict_var(X_test)[:5]
+
+    assert (expected_y_pred_quantile.to_numpy() == y_pred_q.values).all()
+    assert expected_y_pred_quantile.columns == ["__target__0.05__", "__target__0.95__"]
+
+    assert (expected_y_pred_int.to_numpy() == y_pred_i.values).all()
+    assert expected_y_pred_int.columns == [
+        "__target__0.9__lower__",
+        "__target__0.9__upper__",
+    ]
+
+    assert (expected_y_pred_var.to_numpy() == y_pred_v.values).all()
+    assert expected_y_pred_var.columns == ["target"]
