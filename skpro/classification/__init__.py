780aeb305c11ca03308c4ed021733b9e81f10e33
ShreeshaM07 <shreesham2k22@yahoo.com>
2024-07-02 18:24:46 +0530

    removed previous classification folder

--- skpro/classification/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Probabilistic Multiclass Classification."""
--- skpro/classification/sklearn_classifiers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Adapters for probabilistic classifiers, towards sklearn."""
-# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
-
-from skpro.classification.sklearn_classifiers._sklearn_proba_class import (
-    MulticlassSklearnProbaClassifier,
-)
-
-__all__ = ["MulticlassSklearnProbaClassifier"]
