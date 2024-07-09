d3edf9f8f44346a094586a9736a6c5fa795f75cb
ShreeshaM07 <120820143+ShreeshaM07@users.noreply.github.com>
2024-07-10 01:10:24 +0530

    [ENH] Multiclass classification reduction using Histograms (#410)
    #### Reference Issues/PRs
    
    fixes #378 
    
    #### What does this implement/fix? Explain your changes.
    
    It adds an adapter to sklearn multiclass classifiers and expresses them
    as a Histogram probability distribution.

---
+++ skpro/regression/binned/__init__.py
@@ -0,0 +1,6 @@
+"""Reduction to probabilistic classification."""
+# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
+
+from skpro.regression.binned._sklearn_bin_regressor import HistBinnedProbaRegressor
+
+__all__ = ["HistBinnedProbaRegressor"]
