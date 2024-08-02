e038087d87f35d4c3fc03efd1751659117b7af27
Franz Király <f.kiraly@ucl.ac.uk>
2024-08-02 23:01:18 +0100

    Update _base.py

--- skpro/base/_base.py
+++ skpro/base/_base.py
@@ -40,13 +40,13 @@ class BaseObject(_CommonTags, _BaseObject):
         # If yes, remove this msg.
         if sys_version not in py39_or_higher:
             warn(
-                f"From skpro 2.5.0, sktime requires Python version >=3.9, "
+                f"From skpro 2.5.0, skpro requires Python version >=3.9, "
                 f"but found {sys_version}. "
                 "The package can still be installed, until 3.8 end of life "
                 "is reached, "
                 "but some functionality may not work as test coverage is dropped."
                 "Kindly note for context: python 3.8 will reach end of life "
-                "in October 2024, and multiple sktime core dependencies, "
+                "in October 2024, and multiple skpro core dependencies, "
                 "including scikit-learn, have already dropped support for 3.8. ",
                 category=DeprecationWarning,
                 stacklevel=2,
