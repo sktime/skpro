e360e737c8b86c9e624a62e1a6a842415a53427c
Julian Fong <44014224+julian-fong@users.noreply.github.com>
2024-08-18 09:44:02 -0400

    [ENH] Polars adapter enhancements (#449)
    adds index support as part of #440 and is used to sync up polars
    conversion utilities between skpro and sktime.
    
    Correponding sktime pr for polars conversion utilities is
    https://github.com/sktime/sktime/pull/6455/.
    
    In this pr:
    
    If a pandas Dataframe is a `from_type` and polars frame is a `to_type`
    then during the conversion, we will save the index (assumed never to be
    in multi-index format) and insert it as an individual column with column
    name `__index__`. Then the resulting pandas dataframe will be converted
    to a polars dataframe.
    
    In the inverse function, if we are converting from polars dataframe to
    pandas dataframe, if the column `__index__` exists in the pandas
    dataframe post-conversion, then we will map that column to the index
    before returning the pandas Dataframe
    
    After this is merged, #447 will be implemented as a `polars` only
    estimator. tests will also be written to check polars input end to end
    and pandas input and output through the polars estimator (i.e pandas
    input into polars estimator -> polars predictions -> pandas output)

--- skpro/datatypes/tests/test_polars.py
+++ skpro/datatypes/tests/test_polars.py
@@ -50,6 +50,20 @@ def polars_load_diabetes_polars(polars_load_diabetes_pandas):
     X_test_pl = convert_pandas_to_polars_eager(X_test)
     y_train_pl = convert_pandas_to_polars_eager(y_train)
 
+    # drop the index in the polars frame
+    X_train_pl = X_train_pl.drop(["__index__"])
+    X_test_pl = X_test_pl.drop(["__index__"])
+    y_train_pl = y_train_pl.drop(["__index__"])
+
+    return [X_train_pl, X_test_pl, y_train_pl]
+
+
+def polars_load_diabetes_polars_with_index(polars_load_diabetes_pandas):
+    X_train, X_test, y_train = polars_load_diabetes_pandas
+    X_train_pl = convert_pandas_to_polars_eager(X_train)
+    X_test_pl = convert_pandas_to_polars_eager(X_test)
+    y_train_pl = convert_pandas_to_polars_eager(y_train)
+
     return [X_train_pl, X_test_pl, y_train_pl]
 
 
@@ -72,6 +86,7 @@ def test_polars_eager_conversion_methods(
     assert check_polars_table(X_train_pl)
     assert check_polars_table(X_test_pl)
     assert check_polars_table(y_train_pl)
+
     assert (X_train.values == X_train_pl.to_numpy()).all()
     assert (X_test.values == X_test_pl.to_numpy()).all()
     assert (y_train.values == y_train_pl.to_numpy()).all()
