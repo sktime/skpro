5e63fb2ef607ed55a500c27635a04e2d714bbd8d
Franz Király <f.kiraly@ucl.ac.uk>
2024-09-13 23:11:04 +0100

    OnlineRefit

---
+++ skpro/regression/online/__init__.py
@@ -0,0 +1,6 @@
+"""Meta-algorithms to build online regression models."""
+# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
+
+from skpro.regression.online._refit import OnlineRefit
+
+__all__ = ["OnlineRefit"]
