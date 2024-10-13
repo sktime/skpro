d6976ce30a0f482f1decf84a74171f729fa3ee7a
Franz Király <fkiraly@gcos.ai>
2024-10-13 02:06:16 +0200

    [ENH] add suite test for docstring and `get_test_params` coverage (#482)
    This PR adds tests for doctest and estimator `get_test_params`:
    
    * adds a test `test_get_test_params_coverage` that checks for at least
    two test param sets
    * moves doctests from `pytest` doctest flag into a test for classes, in
    `TestAllObjects`, and one for functions. The test for classes is
    conditioned on dependencies, so the doctest skips are no longer
    necessary for dependency isolation.
    
    Mirrors the state of tests in `sktime`.

--- skpro/regression/online/_dont_refit.py
+++ skpro/regression/online/_dont_refit.py
@@ -89,7 +89,7 @@ class OnlineDontRefit(_DelegatedProbaRegressor):
             `MyClass(**params)` or `MyClass(**params[i])` creates a valid test instance.
             `create_test_instance` uses the first (or only) dictionary in `params`
         """
-        from sklearn.linear_model import LinearRegression
+        from sklearn.linear_model import LinearRegression, Ridge
 
         from skpro.regression.residual import ResidualDouble
         from skpro.survival.coxph import CoxPH
@@ -102,5 +102,8 @@ class OnlineDontRefit(_DelegatedProbaRegressor):
         if _check_estimator_deps(CoxPH, severity="none"):
             coxph = CoxPH()
             params.append({"estimator": coxph})
+        else:
+            ridge = Ridge()
+            params.append({"estimator": ResidualDouble(ridge)})
 
         return params
