fc3baac2b06c259a0c34e3c8b544af929af10937
Meraldo Antonio <meraldo@.antonio@gmail.com>
2024-11-22 00:55:52 +0800

    added init

---
+++ skpro/regression/bayesian/__init__.py
@@ -0,0 +1,8 @@
+"""Base classes for Bayesian probabilistic regression."""
+# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
+
+__all__ = ["BayesianConjugateLinearRegressor"]
+
+from skpro.regression.bayesian.bayesian_conjugate import (
+    BayesianConjugateLinearRegressor,
+)
