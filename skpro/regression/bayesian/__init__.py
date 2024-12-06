5e37db3f0e3b185f1d81abc93255dd93a701b3b3
Meraldo Antonio <meraldo@.antonio@gmail.com>
2024-12-06 15:36:08 +0800

    Added init

---
+++ skpro/regression/bayesian/__init__.py
@@ -0,0 +1,8 @@
+"""Base classes for Bayesian probabilistic regression."""
+# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
+
+__all__ = ["BayesianMCMCLinearRegressor"]
+
+from skpro.regression.bayesian.bayesian_mcmc import (
+    BayesianMCMCLinearRegressor,
+)
