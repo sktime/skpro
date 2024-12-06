337780d4acdab18cf631fd0328f0208f3525ca5d
Meraldo Antonio <meraldo@.antonio@gmail.com>
2024-12-06 19:48:46 +0800

    changed estimator name

--- skpro/regression/bayesian/__init__.py
+++ skpro/regression/bayesian/__init__.py
@@ -1,6 +1,6 @@
 """Base classes for Bayesian probabilistic regression."""
 # copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
 
-__all__ = ["BayesianMCMCLinearRegressor"]
+__all__ = ["BayesianLinearRegressor"]
 
-from skpro.regression.bayesian.bayesian_mcmc import BayesianMCMCLinearRegressor
+from skpro.regression.bayesian._bayesian_mcmc import BayesianLinearRegressor
