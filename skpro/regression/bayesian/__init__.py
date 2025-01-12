dbfdcea5ccc622700d84bfeff3d523c1dcb3ed2d
Franz Király <f.kiraly@ucl.ac.uk>
2025-01-12 17:21:06 +0100

    folders

--- skpro/regression/bayesian/__init__.py
+++ skpro/regression/bayesian/__init__.py
@@ -1,8 +1,12 @@
 """Base classes for Bayesian probabilistic regression."""
 # copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
 
-__all__ = ["BayesianConjugateLinearRegressor"]
+__all__ = [
+    "BayesianConjugateLinearRegressor",
+    "BayesianLinearRegressor",
+]
 
-from skpro.regression.bayesian._bayesian_conjugate import (
+from skpro.regression.bayesian._linear_conjugate import (
     BayesianConjugateLinearRegressor,
 )
+from skpro.regression.bayesian._linear_mcmc import BayesianLinearRegressor
