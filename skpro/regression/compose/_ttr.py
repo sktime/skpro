2c5ff2ab6c7d85f78d357e115644a227027f4ce8
Franz Király <fkiraly@gcos.ai>
2025-06-28 21:37:28 +0200

    Create _ttr.py

---
+++ skpro/regression/compose/_ttr.py
@@ -0,0 +1,387 @@
+"""Implements transformed target regressor for probabilistic supervised regression."""
+# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
+
+__author__ = ["fkiraly"]
+__all__ = ["TransformedTargetRegressor"]
+
+import pandas as pd
+from sklearn import clone
+
+from skpro.distributions.trafo import TransformedDistribution
+from skpro.regression.base import BaseProbaRegressor
+from skpro.utils.sklearn import prep_skl_df
+
+
+class TransformedTargetRegressor(BaseProbaRegressor):
+    """Pipeline for probabilistic supervised regression.
+
+    Pipeline is only applying the given transformers
+    to X. The regressor can also be a TransformedTargetregressor containing
+    transformers to transform y.
+
+    For a list `t1`, `t2`, ..., `tN`, `r`
+        where `t[i]` are transformers, and `r` is an sktime regressor,
+        the pipeline behaves as follows:
+
+    `fit(X, y)` - changes state by running `t1.fit_transform` with `X=X`, `y=y`
+        then `t2.fit_transform` on `X=` the output of `t1.fit_transform`, `y=y`, etc
+        sequentially, with `t[i]` receiving the output of `t[i-1]` as `X`,
+        then running `r.fit` with `X` being the output of `t[N]`, and `y=y`
+    `predict(X)` - result is of executing `r.predict`, with `X=X`
+        being the result of the following process:
+        running `t1.fit_transform` with `X=X`,
+        then `t2.fit_transform` on `X=` the output of `t1.fit_transform`, etc
+        sequentially, with `t[i]` receiving the output of `t[i-1]` as `X`,
+        and returning th output of `tN` to pass to `r.predict` as `X`.
+    `predict_interval(X)`, `predict_quantiles(X)` - as `predict(X)`,
+        with `predict_interval` or `predict_quantiles` substituted for `predict`
+    `predict_var`, `predict_proba` - uses base class default to obtain
+        crude estimates from `predict_quantiles`.
+
+    `get_params`, `set_params` uses `sklearn` compatible nesting interface
+        if list is unnamed, names are generated as names of classes
+        if names are non-unique, `f"_{str(i)}"` is appended to each name string
+            where `i` is the total count of occurrence of a non-unique string
+            inside the list of names leading up to it (inclusive)
+
+    `Pipeline` can also be created by using the magic multiplication
+        on any regressor, i.e., if `my_regressor` inherits from `BaseProbaRegressor`,
+            and `my_t1`, `my_t2`, are an `sklearn` transformer,
+            then, for instance, `my_t1 * my_t2 * my_regressor`
+            will result in the same object as  obtained from the constructor
+            `Pipeline([my_t1, my_t2, my_regressor])`
+        magic multiplication can also be used with (str, transformer) pairs,
+            as long as one element in the chain is a regressor
+
+    Parameters
+    ----------
+    steps : list of sktime transformers and regressors, or
+        list of tuples (str, estimator) of sktime transformers or regressors
+            the list must contain exactly one regressor
+        these are "blueprint" transformers resp regressors,
+            regressor/transformer states do not change when `fit` is called
+
+    Attributes
+    ----------
+    steps_ : list of tuples (str, estimator) of sktime transformers or regressors
+        clones of estimators in `steps` which are fitted in the pipeline
+        is always in (str, estimator) format, even if `steps` is just a list
+        strings not passed in `steps` are replaced by unique generated strings
+        i-th transformer in `steps_` is clone of i-th in `steps`
+    regressor_ : estimator, reference to the unique regressor in steps_
+
+    Examples
+    --------
+    >>> from skpro.regression.compose import Pipeline
+    >>> from skpro.regression.residual import ResidualDouble
+    >>> from sklearn.datasets import load_diabetes
+    >>> from sklearn.impute import SimpleImputer as Imputer
+    >>> from sklearn.linear_model import LinearRegression
+    >>> from sklearn.model_selection import train_test_split
+    >>> from sklearn.preprocessing import MinMaxScaler
+    >>>
+    >>> X, y = load_diabetes(return_X_y=True, as_frame=True)
+    >>> X_train, X_test, y_train, y_test = train_test_split(X, y)
+    >>>
+    >>> reg_mean = LinearRegression()
+    >>> reg_proba = ResidualDouble(reg_mean)
+
+    Example 1: string/estimator pairs
+
+    >>> pipe = Pipeline(steps=[
+    ...     ("imputer", Imputer()),
+    ...     ("scaler", MinMaxScaler()),
+    ...     ("regressor", reg_proba),
+    ... ])
+    >>> pipe.fit(X_train, y_train)
+    Pipeline(...)
+    >>> y_pred = pipe.predict(X=X_test)
+    >>> y_pred_proba = pipe.predict_proba(X=X_test)
+
+    Example 2: without strings
+
+    >>> pipe = Pipeline([
+    ...     Imputer(),
+    ...     MinMaxScaler(),
+    ...     ("regressor", reg_proba),
+    ... ])
+
+    Example 3: using the dunder method
+    (requires bracketing as sklearn does not support dunders)
+
+    >>> reg_proba = ResidualDouble(reg_mean)
+    >>> pipe = Imputer() * (MinMaxScaler() * reg_proba)
+    """
+
+    _tags = {
+        "capability:multioutput": True,
+        "capability:missing": True,
+    }
+
+    def __init__(self, regressor, transformer=None):
+        self.regressor = regressor
+        self.transformer = transformer
+        super().__init__()
+
+        self.regressor_ = regressor.clone()
+        self.transformer_ = clone(transformer) if transformer else None
+
+        tags_to_clone = [
+            "capability:multioutput",
+            "capability:survival",
+            "capability:update",
+        ]
+        self.clone_tags(self.regressor_, tags_to_clone)
+
+    def _fit(self, X, y, C=None):
+        """Fit regressor to training data.
+
+        Writes to self:
+            Sets fitted model attributes ending in "_".
+
+        Parameters
+        ----------
+        X : pandas DataFrame
+            feature instances to fit regressor to
+        y : pd.DataFrame, must be same length as X
+            labels to fit regressor to
+        C : pd.DataFrame, optional (default=None)
+            censoring information for survival analysis,
+            should have same column name as y, same length as X and y
+            should have entries 0 and 1 (float or int)
+            0 = uncensored, 1 = (right) censored
+            if None, all observations are assumed to be uncensored
+            Can be passed to any probabilistic regressor,
+            but is ignored if capability:survival tag is False.
+
+        Returns
+        -------
+        self : reference to self
+        """
+        # coerce X to pandas DataFrame with string column names
+        X = prep_skl_df(X, copy_df=True)
+
+        # transform y
+        if self.transformer_ is not None:
+            t = self.transformer_
+            yt = t.fit_transform(y=y)
+            if not isinstance(yt, pd.DataFrame):
+                yt = pd.DataFrame(yt, index=y.index)
+        else:
+            yt = y
+
+        # fit regressor
+        regressor = self.regressor_
+        regressor.fit(X, yt, C=C)
+
+        return self
+
+    def _update(self, X, y, C=None):
+        """Update regressor with a new batch of training data.
+
+        State required:
+            Requires state to be "fitted" = self.is_fitted=True
+
+        Writes to self:
+            Updates fitted model attributes ending in "_".
+
+        Parameters
+        ----------
+        X : pandas DataFrame
+            feature instances to fit regressor to
+        y : pd.DataFrame, must be same length as X
+            labels to fit regressor to
+        C : pd.DataFrame, optional (default=None)
+            censoring information for survival analysis,
+            should have same column name as y, same length as X and y
+            should have entries 0 and 1 (float or int)
+            0 = uncensored, 1 = (right) censored
+            if None, all observations are assumed to be uncensored
+            Can be passed to any probabilistic regressor,
+            but is ignored if capability:survival tag is False.
+
+        Returns
+        -------
+        self : reference to self
+        """
+        yt = self.transformer_(y)
+        self.regressor_.update(X=X, y=yt, C=C)
+        return self
+
+    def _predict(self, X):
+        """Predict labels for data from features.
+
+        State required:
+            Requires state to be "fitted" = self.is_fitted=True
+
+        Accesses in self:
+            Fitted model attributes ending in "_"
+
+        Parameters
+        ----------
+        X : pandas DataFrame, must have same columns as X in `fit`
+            data to predict labels for
+
+        Returns
+        -------
+        y : pandas DataFrame, same length as `X`, same columns as `y` in `fit`
+            labels predicted for `X`
+        """
+        y_pred = self.regressor_.predict(X=X)
+        y_pred_it = self.transformer_.inverse_transform(y_pred)
+        if not isinstance(y_pred_it, pd.DataFrame):
+            y_cols = self._y_metadata["feature_names"]
+            y_pred_it = pd.DataFrame(y_pred_it, index=X.index, columns=y_cols)
+        return y_pred_it
+
+    def _predict_quantiles(self, X, alpha):
+        """Compute/return quantile predictions.
+
+        private _predict_quantiles containing the core logic,
+            called from predict_quantiles and default _predict_interval
+
+        Parameters
+        ----------
+        X : pandas DataFrame, must have same columns as X in `fit`
+            data to predict labels for
+        alpha : guaranteed list of float
+            A list of probabilities at which quantile predictions are computed.
+
+        Returns
+        -------
+        quantiles : pd.DataFrame
+            Column has multi-index: first level is variable name from ``y`` in fit,
+                second level being the values of alpha passed to the function.
+            Row index is equal to row index of ``X``.
+            Entries are quantile predictions, for var in col index,
+                at quantile probability in second col index, for the row index.
+        """
+        y_pred = self.regressor_.predict_quantiles(X=X)
+        y_pred_it = self._get_inverse_transform_pred_int(
+            transformer=self.transformer_, y=y_pred
+        )
+        return y_pred_it
+
+    def _predict_interval(self, X, coverage):
+        """Compute/return interval predictions.
+
+        private _predict_interval containing the core logic,
+            called from predict_interval and default _predict_quantiles
+
+        Parameters
+        ----------
+        X : pandas DataFrame, must have same columns as X in `fit`
+            data to predict labels for
+        coverage : guaranteed list of float of unique values
+           nominal coverage(s) of predictive interval(s)
+
+        Returns
+        -------
+        pred_int : pd.DataFrame
+            Column has multi-index: first level is variable name from ``y`` in fit,
+            second level coverage fractions for which intervals were computed,
+            in the same order as in input `coverage`.
+            Third level is string "lower" or "upper", for lower/upper interval end.
+            Row index is equal to row index of ``X``.
+            Entries are lower/upper bounds of interval predictions,
+            for var in col index, at nominal coverage in second col index,
+            lower/upper depending on third col index, for the row index.
+            Upper/lower interval end are equivalent to
+            quantile predictions at alpha = 0.5 - c/2, 0.5 + c/2 for c in coverage.
+        """
+        y_pred = self.regressor_.predict_quantiles(X=X)
+        y_pred_it = self._get_inverse_transform_pred_int(
+            transformer=self.transformer_, y=y_pred
+        )
+        return y_pred_it
+
+    def _predict_var(self, X):
+        """Compute/return variance predictions.
+
+        private _predict_var containing the core logic, called from predict_var
+
+        Parameters
+        ----------
+        X : pandas DataFrame, must have same columns as X in `fit`
+            data to predict labels for
+
+        Returns
+        -------
+        pred_var : pd.DataFrame
+            Column names are exactly those of ``y`` passed in ``fit``.
+            Row index is equal to row index of ``X``.
+            Entries are variance prediction, for var in col index.
+            A variance prediction for given variable and fh index is a predicted
+            variance for that variable and index, given observed data.
+        """
+        # explicitly using default - should be obtained from predict_proba
+        return super()._predict_var(X=X)
+
+    def _predict_proba(self, X):
+        """Predict distribution over labels for data from features.
+
+        State required:
+            Requires state to be "fitted".
+
+        Accesses in self:
+            Fitted model attributes ending in "_"
+
+        Parameters
+        ----------
+        X : pandas DataFrame, must have same columns as X in `fit`
+            data to predict labels for
+
+        Returns
+        -------
+        y : skpro BaseDistribution, same length as `X`
+            labels predicted for `X`
+        """
+        y_pred = self.regressor_.predict_quantiles(X=X)
+        y_pred_it = TransformedDistribution(
+            distribution=y_pred,
+            transform=self.transformer_,
+            assume_monotonic=True,
+            index=X.index,
+            columns=self._y_metadata["feature_names"]
+        )
+        return y_pred_it
+
+    def _get_inverse_transform_pred_int(self, transformer, y):
+        """Inverse transform predict outputs predict_interval and predict_quantiles.
+
+        Parameters
+        ----------
+        transformer : sklearn transformer
+            Transformer to inverse transform y
+        y : pd.DataFrame
+            Target series
+
+        Returns
+        -------
+        y : pd.DataFrame
+            Inverse transformed y
+        """
+        # if proba, we slice by quantile/coverage combination
+        #   and collect the same quantile/coverage by variable
+        #   then inverse transform, then concatenate
+        idx = y.columns
+        n = idx.nlevels
+        idx_low = idx.droplevel(0).unique()
+        yt = dict()
+        for ix in idx_low:
+            levels = list(range(1, n))
+            if len(levels) == 1:
+                levels = levels[0]
+            yt[ix] = y.xs(ix, level=levels, axis=1)
+            if len(yt[ix].columns) == 1:
+                temp = yt[ix].columns
+                yt[ix].columns = self._y.columns
+            yt[ix] = transformer.inverse_transform(X=yt[ix])
+            if len(yt[ix].columns) == 1:
+                yt[ix].columns = temp
+        y = pd.concat(yt, axis=1)
+        flipcols = [n - 1] + list(range(n - 1))
+        y.columns = y.columns.reorder_levels(flipcols)
+        y = y.loc[:, idx]
+
+        return y
