f2d149b70cb80336dcb84effc754dde32bcd1ee6
Victor <victor.tingstrom@gmail.com>
2025-06-28 13:10:09 +0200

    Adds array case for Lefttruncated

--- skpro/distributions/discrete_truncated.py
+++ skpro/distributions/discrete_truncated.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 from skpro.distributions import Uniform
 from skpro.distributions.base import BaseDistribution
@@ -70,10 +71,50 @@ class LeftTruncatedDiscrete(BaseDistribution):
     def get_test_params(cls, parameter_set="default"):
         from skpro.distributions import Poisson
 
+        # scalar
         poisson = Poisson(mu=1.0)
         params1 = {
             "distribution": poisson,
             "lower_bound": 0,
         }
 
-        return [params1]
+        # array
+        idx = pd.Index([1, 2])
+        cols = pd.Index(["a", "b"])
+        n_array = Poisson(mu=[[1, 2], [3, 4]], columns=cols, index=idx)
+        params2 = {
+            "distribution": n_array,
+            "lower_bound": 0,
+            "index": idx,
+            "columns": cols,
+        }
+
+        return [params1, params2]
+
+    def _iloc(self, rowidx=None, colidx=None):
+        distr = self.distribution.iloc[rowidx, colidx]
+
+        if rowidx is not None:
+            new_index = self.index[rowidx]
+        else:
+            new_index = self.index
+
+        if colidx is not None:
+            new_columns = self.columns[colidx]
+        else:
+            new_columns = self.columns
+
+        cls = type(self)
+        return cls(
+            distribution=distr,
+            lower_bound=self.lower_bound,
+            index=new_index,
+            columns=new_columns,
+        )
+
+    def _iat(self, rowidx=None, colidx=None):
+        if rowidx is None or colidx is None:
+            raise ValueError("iat method requires both row and column index")
+        self_subset = self.iloc[[rowidx], [colidx]]
+
+        return type(self)(distribution=self_subset.distribution.iat[0, 0], lower_bound=self.lower_bound)
