437c355c9bae3bfbea8af2f8dd81cf818decfe32
Joshua Dunn <j.dunn@cfp.energy>
2025-10-03 21:35:12 +0100

    feat: Implement more of the sklearn -> skpro transformer logic

---
+++ skpro/compose/__init__.py
@@ -0,0 +1,13 @@
+"""Composition utilities for skpro."""
+
+__all__ = [
+    "BaseTransformer",
+    "BaseDifferentiableTransformer",
+    "DifferentiableTransformer",
+]
+
+from skpro.compose._transformer import (
+    BaseDifferentiableTransformer,
+    BaseTransformer,
+    DifferentiableTransformer,
+)
