197f3e0db4da197154442235b23a281316c3149c
ravjot07 <ravu2004@gmail.com>
2025-11-22 14:12:55 +0530

    modified logic and strcture
    Signed-off-by: ravjot07 <ravu2004@gmail.com>

--- skpro/regression/adapters/pygam/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Adapter to pyGAM probabilistic regressors."""
-# copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
-
-from skpro.regression.adapters.pygam._pygam_proba import PyGAMAdapter
-
-__all__ = ["PyGAMAdapter"]
