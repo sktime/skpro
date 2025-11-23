5516094940a8763c52728dc9ba837fb70d3b1b64
ravjot07 <ravu2004@gmail.com>
2025-11-23 13:05:47 +0530

    [ENH] Add OnlineBatchMixture and OnlineBootstrapRemember (#464)
    Signed-off-by: ravjot07 <ravu2004@gmail.com>

--- skpro/regression/online/__init__.py
+++ skpro/regression/online/__init__.py
@@ -1,8 +1,16 @@
 """Meta-algorithms to build online regression models."""
 # copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
 
+from skpro.regression.online._batch_mixture import OnlineBatchMixture
+from skpro.regression.online._bootstrap_remember import OnlineBootstrapRemember
 from skpro.regression.online._dont_refit import OnlineDontRefit
 from skpro.regression.online._refit import OnlineRefit
 from skpro.regression.online._refit_every import OnlineRefitEveryN
 
-__all__ = ["OnlineDontRefit", "OnlineRefit", "OnlineRefitEveryN"]
+__all__ = [
+    "OnlineBatchMixture",
+    "OnlineBootstrapRemember",
+    "OnlineDontRefit",
+    "OnlineRefit",
+    "OnlineRefitEveryN",
+]
