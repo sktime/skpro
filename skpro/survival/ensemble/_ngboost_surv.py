937825e2bf17d566e4941c36e21f23e537fe1171
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-12-12 09:34:30 +0530

    Update _ngboost_surv.py

--- skpro/survival/ensemble/_ngboost_surv.py
+++ skpro/survival/ensemble/_ngboost_surv.py
@@ -131,7 +131,7 @@ class NGBoostSurvival(BaseSurvReg, NGBoostAdapter):
         # ngboost => 1 = uncensored, else (right) censored
         # If C is None then C is set as 1s (uncensored)
         # else it is converted from skpro to ngboost format
-        # by doing C = 1-C        
+        # by doing C = 1-C
         if C is None:
             C = pd.DataFrame(np.ones(len(y)), index=y.index, columns=y.columns)
         else:
