fd4aa2034bb82a770de65c311e39661e12cbccd9
Arnav Kapoor <kapoorarnav43@gmail.com>
2026-02-18 18:31:53 +0530

    readded pdfnorm to _transformed.py to enable fallback to numerical differentiation of cdf for pdfnorm computation

--- skpro/distributions/trafo/_transformed.py
+++ skpro/distributions/trafo/_transformed.py
@@ -105,11 +105,13 @@ class TransformedDistribution(BaseDistribution):
 
         # if inverse_transform is given, we can do exact cdf
         # due to the formula F_g(x)(y) = F_X(g^-1(x))
+        # and approximate pdf via numerical differentiation of cdf
+        # which enables pdfnorm computation
         if self.inverse_transform is not None:
             self.set_tags(
                 **{
                     "capabilities:exact": ["ppf", "cdf"],
-                    "capabilities:approx": ["mean", "var", "energy"],
+                    "capabilities:approx": ["pdf", "pdfnorm", "mean", "var", "energy"],
                 }
             )
 
