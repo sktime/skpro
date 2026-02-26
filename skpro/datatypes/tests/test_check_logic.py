d892a483c5f6a71bee40388cb7e516f10436f814
kabirvashisht4-glitch <kabirvashisht4@gmail.com>
2026-02-26 16:43:53 +0530

    fixed test_check_logic.py file

--- skpro/datatypes/tests/test_check_logic.py
+++ skpro/datatypes/tests/test_check_logic.py
@@ -1,7 +1,10 @@
 from skpro.datatypes._table._check import _check_list_of_dict_table
 
+
 def test_check_list_of_dict_table_detects_invalid_types():
     invalid_data = [{"a": 1}, {"a": [99]}]
+
     check_result, msg, _ = _check_list_of_dict_table(invalid_data, return_metadata=True)
+
     assert check_result is False
     assert "not a primitive type" in msg
