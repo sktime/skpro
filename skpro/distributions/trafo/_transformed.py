e748f21017e98db09ef78c0035f560875dfd308a
Franz Király <fkiraly@gcos.ai>
2026-03-01 22:25:17 +0100

    Update _transformed.py

--- skpro/distributions/trafo/_transformed.py
+++ skpro/distributions/trafo/_transformed.py
@@ -105,8 +105,6 @@ class TransformedDistribution(BaseDistribution):
 
         # if inverse_transform is given, we can do exact cdf
         # due to the formula F_g(x)(y) = F_X(g^-1(x))
-        # and approximate pdf via numerical differentiation of cdf
-        # which enables pdfnorm computation
         if self.inverse_transform is not None:
             self.set_tags(
                 **{
