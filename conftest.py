cf703c798370f710048a5cae745f123c3cae643a
Arnav Kapoor <kapoorarnav43@gmail.com>
2026-03-04 16:33:30 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -14,14 +14,14 @@ __author__ = ["fkiraly"]
 
 import os
 
+from skbase.utils.dependencies import _check_soft_dependencies
+
 # used to prevent tkinter related errors in CI
-if os.environ.get("GITHUB_ACTIONS") == "true":
-    try:
+if _check_soft_dependencies("matplotlib", severity="none"):
+    if os.environ.get("GITHUB_ACTIONS") == "true":
         import matplotlib
 
         matplotlib.use("Agg")
-    except ImportError:
-        pass
 
 
 def pytest_addoption(parser):
