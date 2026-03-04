ed12362cb1a9f398e9128f45dbdfa1fe1fe349dc
Arnav Kapoor <kapoorarnav43@gmail.com>
2026-03-04 15:45:50 +0530

    [ENH] Add BayesianProportionEstimator for Bayesian proportion estimation
    - New estimator for estimating proportions using Bayesian inference
    - Uses Beta prior with Binomial likelihood
    - Provides posterior distribution over proportions
    - Supports sequential updating as new data arrives
    - From PR #417: Conjugate Bayes for proportion

--- skpro/regression/__init__.py
+++ skpro/regression/__init__.py
@@ -1,5 +1,6 @@
 """Probabilitistic supervised regression estimators."""
 
+from skpro.regression.bayesian_proportion import BayesianProportionEstimator
 from skpro.regression.conformal import (
     MapieConformalizedQuantileRegressor,
     MapieCrossConformalRegressor,
@@ -8,6 +9,7 @@ from skpro.regression.conformal import (
 from skpro.regression.jackknife import MapieJackknifeAfterBootstrapRegressor
 
 __all__ = [
+    "BayesianProportionEstimator",
     "MapieSplitConformalRegressor",
     "MapieCrossConformalRegressor",
     "MapieConformalizedQuantileRegressor",
