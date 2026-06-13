5b9a862ffa063b8c88ed38969bcac58ac14b5bab
Chaitany patel <patelchaitany93@gmail.com>
2026-06-14 03:07:48 +0530

    Add ConformalResidualRegressor

--- skpro/regression/base/__init__.py
+++ skpro/regression/base/__init__.py
@@ -1,8 +1,7 @@
 """Base classes for probabilistic regression."""
 # copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
 
-__all__ = ["BaseOnlineRegressor", "BaseProbaRegressor", "_DelegatedProbaRegressor"]
+__all__ = ["BaseProbaRegressor", "_DelegatedProbaRegressor"]
 
 from skpro.regression.base._base import BaseProbaRegressor
 from skpro.regression.base._delegate import _DelegatedProbaRegressor
-from skpro.regression.base._online import BaseOnlineRegressor
--- skpro/regression/enbpi.py
+++ skpro/regression/enbpi.py
@@ -32,6 +32,10 @@ class EnbpiRegressor(BaseProbaRegressor):
     The clones are aggregated to predict quantiles of the target distribution,
     following the original algorithms in [1]_ and [2]_.
 
+    Supports online learning via ``update``: each bootstrap clone is refit on a
+    bootstrap subsample of the new batch, and jackknife+ residuals are appended
+    to the conformity score pool.
+
     The parameters in the reference [2]_ are mapped to the parameters of the
     estimator as follows: :math:`\mathcal{A}` is ``estimator``, :math:`B` is
     ``n_bootstrap_samples``, :math:`x_i, i = 1, \dots, T` are the rows of
@@ -99,12 +103,15 @@ class EnbpiRegressor(BaseProbaRegressor):
     >>> reg_proba = EnbpiRegressor(reg_tabular)
     >>> reg_proba.fit(X_train, y_train)
     EnbpiRegressor(...)
+    >>> reg_proba.update(X_test[:10], y_test[:10].to_frame())
+    EnbpiRegressor(...)
     >>> y_pred = reg_proba.predict_proba(X_test)
     """
 
     _tags = {
         "authors": ["fkiraly", "hamrel-cxu"],
         "capability:missing": True,
+        "capability:update": True,
     }
 
     def __init__(
@@ -165,17 +172,60 @@ class EnbpiRegressor(BaseProbaRegressor):
         inst_ix = X.index
         n = len(inst_ix)
 
-        self.estimators_ = []
         self._cols = y.columns
 
-        # coerce X to pandas DataFrame with string column names
+        estimators = [clone(estimator) for _ in range(n_bootstrap_samples)]
+        errs, bs_vs_ix, estimators = self._bootstrap_batch(X, y, estimators)
+
+        self.estimators_ = estimators
+        self._errs = errs
+        self._bs_vs_ix = bs_vs_ix
+        self._n_train = n
+
+        return self
+
+    def _update(self, X, y, C=None):
+        """Update regressor with a new batch of training data.
+
+        Each bootstrap clone is refit on a bootstrap subsample of the new batch.
+        Jackknife+ residuals from the new batch are appended to the stored
+        conformity scores, following the online EnbPI algorithm in [2]_.
+
+        Parameters
+        ----------
+        X : pandas DataFrame
+            feature instances to update with
+        y : pandas DataFrame, must be same length as X
+            labels to update with
+
+        Returns
+        -------
+        self : reference to self
+        """
+        errs, bs_vs_ix_new, estimators = self._bootstrap_batch(X, y, self.estimators_)
+        self.estimators_ = estimators
+
+        n_new = len(X)
+        self._bs_vs_ix = np.pad(self._bs_vs_ix, ((0, n_new), (0, 0)), constant_values=0)
+        self._bs_vs_ix[-n_new:, :] = bs_vs_ix_new
+        self._errs = np.concatenate([self._errs, errs], axis=0)
+        self._n_train += n_new
+
+        return self
+
+    def _bootstrap_batch(self, X, y, estimators):
+        """Bootstrap-fit clones on a batch and compute jackknife+ residuals."""
+        n_bootstrap_samples = self.n_bootstrap_samples
+        inst_ix = X.index
+        n = len(inst_ix)
+
         X = prep_skl_df(X, copy_df=True)
 
-        y_pred_bs = np.ones((n_bootstrap_samples,) + y.shape) * np.nan
+        y_pred_bs = np.ones((n_bootstrap_samples, n, y.shape[1])) * np.nan
         bs_vs_ix = np.zeros((n, n_bootstrap_samples))
 
         for i in range(n_bootstrap_samples):
-            esti = clone(estimator)
+            esti = estimators[i]
             row_iloc = pd.RangeIndex(n)
             row_ss = _random_ss_ix(
                 row_iloc, size=n, replace=self.replace, random_state=self._random_state
@@ -189,12 +239,12 @@ class EnbpiRegressor(BaseProbaRegressor):
             yi = y.loc[inst_ix_i].values
             yi = flatten_to_1D_if_colvector(yi)
 
-            self.estimators_ += [esti.fit(Xi, yi)]
+            estimators[i] = esti.fit(Xi, yi)
 
             y_pred = esti.predict(X)
-            y_pred_bs[[i]] = _coerce_numpy2d(y_pred)
+            y_pred_bs[i] = _coerce_numpy2d(y_pred)
 
-        y_pred_insample = np.ones(y.shape) * np.nan
+        y_pred_insample = np.ones((n, y.shape[1])) * np.nan
 
         for i in range(n):
             y_pred_insample[[i], :] = self._pred_phi_sans_i(
@@ -205,11 +255,7 @@ class EnbpiRegressor(BaseProbaRegressor):
         if self.symmetrize:
             errs = np.abs(errs)
 
-        self._errs = errs
-        self._bs_vs_ix = bs_vs_ix
-        self._n_train = n
-
-        return self
+        return errs, bs_vs_ix, estimators
 
     def _pred_phi_sans_i(self, y_preds, i, bs_vs_ix):
         # y_preds - (n_bootstrap_samples, n_samples, n_vars)
