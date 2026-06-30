607dd20ff7c0fc858309540e3361e7c1463113f5
Franz Király <fkiraly@gcos.ai>
2026-07-01 01:23:33 +0200

    Release 2.14.0 (#1091)
    Release PR for 2.14.0
    
    * changelog
    * version bump
    * tags updated

--- skpro/regression/ondil.py
+++ skpro/regression/ondil.py
@@ -80,7 +80,7 @@ class OndilOnlineGamlss(BaseProbaRegressor):
 
             warn(
                 "in `OndilOnlineGamlss`, parameter 'distribution' "
-                "will be renamed to 'dist' in version 2.14.0. "
+                "will be renamed to 'dist' in version 2.15.0. "
                 "To keep current behaviour and to silence this warning, "
                 "use 'dist' instead of 'distribution', "
                 "set dist explicitly via kwarg, and do not set distribution.",
