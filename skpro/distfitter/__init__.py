89582c0e6c180cfb3b7a2a5186d2cb384ddc671f
areychana <93970069+areychana@users.noreply.github.com>
2026-07-02 08:52:16 +0530

    Add DistfitFitter distribution fitter wrapping the distfit package

--- skpro/distfitter/__init__.py
+++ skpro/distfitter/__init__.py
@@ -1,10 +1,12 @@
 """Distribution fitter estimators."""
 # copyright: skpro developers, BSD-3-Clause License (see LICENSE file)
 
+from skpro.distfitter._distfitfitter import DistfitFitter
 from skpro.distfitter._momfitter import MOMFitter
 from skpro.distfitter._normalfitter import NormalFitter
 
 __all__ = [
+    "DistfitFitter",
     "NormalFitter",
     "MOMFitter",
 ]
